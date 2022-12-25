0b61d59e156b36a7e1fe3102f071244df8b9271c
KayAhn0126 <dkseotn123@gmail.com>
2022-12-25 22:08:08 +0900

    refactor: 한국이 그리울 땐 서버에 접속하지 문제를 정규표현식으로 해결

--- Implementation/WhenIMissKoreaConnectToServer/WhenIMissKoreaConnectToServer/WhenIMissKoreaConnectToServer/SolveWithRegex.swift
+++ Implementation/WhenIMissKoreaConnectToServer/WhenIMissKoreaConnectToServer/WhenIMissKoreaConnectToServer/SolveWithRegex.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+func checkPattern(_ testArray: [String], startString: String, endString: String) {
+    let pattern = "^\(startString)[a-z]*\(endString)$"
+
+    testArray.enumerated().forEach {
+        let regex = try? NSRegularExpression(pattern: pattern)
+        let range = NSRange(location: 0, length: $0.element.count)
+        if regex?.matches(in: $0.element, range: range) != Optional([]) {
+            print("DA")
+        } else {
+            print("NE")
+        }
+    }
+}
--- Implementation/WhenIMissKoreaConnectToServer/WhenIMissKoreaConnectToServer/WhenIMissKoreaConnectToServer/main.swift
+++ Implementation/WhenIMissKoreaConnectToServer/WhenIMissKoreaConnectToServer/WhenIMissKoreaConnectToServer/main.swift
@@ -5,7 +5,25 @@
 //  Created by Kay on 2022/12/25.
 //
 
+/*
+ 9996
+ 한국이 그리울 땐 서버에 접속하지
+ */
+
+// 1st week fast-campus
 import Foundation
 
-print("Hello, World!")
+var number = Int(readLine()!)!
+var pattern = readLine()!
+var starIndex = pattern.firstIndex(of: "*")!
+var checkArr: [String] = []
 
+for _ in 0..<number {
+    checkArr.append(readLine()!)
+}
+
+
+let startString = String(pattern[pattern.startIndex..<starIndex])
+let endString = String(pattern[pattern.index(after: starIndex)...pattern.index(before: pattern.endIndex)])
+
+checkPattern(checkArr, startString: startString, endString: endString)
