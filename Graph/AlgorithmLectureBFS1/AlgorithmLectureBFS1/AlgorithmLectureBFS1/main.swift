6c35ce354e21e61b8c310a8e90ad032846d5ce44
KayAhn0126 <dkseotn123@gmail.com>
2023-01-04 19:20:06 +0900

    suspend: 시간초과 문제 미해결

---
+++ Graph/AlgorithmLectureBFS1/AlgorithmLectureBFS1/AlgorithmLectureBFS1/main.swift
@@ -0,0 +1,87 @@
+//
+//  main.swift
+//  AlgorithmLectureBFS1
+//
+//  Created by Kay on 2023/01/04.
+//
+
+/*
+ 24444
+ 알고리즘 수업 - 너비 우선 탐색 1
+ */
+
+// 양방향
+// 가중치 1
+// 인접 정점 오름차순으로 방문 adjMatrix[node].sorted()
+// 정점의 방문 순서 R
+// i번째 줄에 i의 방문 순서 출력
+
+import Foundation
+
+struct Queue<T> {
+    private var queue: [T] = []
+    
+    var count: Int {
+        return queue.count
+    }
+    
+    var isEmpty: Bool {
+        return queue.isEmpty
+    }
+    
+    mutating func enqueue(_ element: T) {
+        queue.append(element)
+    }
+    
+    // isEmpty로 항상 체크하기
+    mutating func dequeue() -> T {
+        queue = queue.reversed()
+        let number = queue.popLast()!
+        queue = queue.reversed()
+        return number
+    }
+}
+
+
+var bfsQueue = Queue<Int>()
+let userInput = readLine()!.split(separator: " ").map { Int($0)! }
+
+let N = userInput[0]
+let M = userInput[1]
+let R = userInput[2]
+
+var adjMatrix = [[Int]](repeating: [Int](), count: N + 1)
+var visited = [Int](repeating: 0, count: N + 1)
+
+var counter = 1
+
+for _ in 0..<M {
+    let edge = readLine()!.split(separator: " ").map { Int($0)! }
+    let u = edge[0]
+    let v = edge[1]
+    adjMatrix[u].append(v)
+    adjMatrix[v].append(u)
+}
+
+func bfs(_ node: Int) {
+    visited[node] = counter
+    bfsQueue.enqueue(node)
+    while !bfsQueue.isEmpty {
+        let currentNode = bfsQueue.dequeue()
+        adjMatrix[currentNode].sort()
+        for i in 0..<adjMatrix[currentNode].count {
+            if visited[adjMatrix[currentNode][i]] != 0 { continue }
+            counter += 1
+            visited[adjMatrix[currentNode][i]] = counter
+            bfsQueue.enqueue(adjMatrix[currentNode][i])
+        }
+    }
+}
+
+bfs(R)
+
+
+for i in 1..<N+1 {
+    print(visited[i])
+}
+
