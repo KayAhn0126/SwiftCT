1401b597bb8600d854f51bf6bd68978eebe7c0f1
KayAhn0126 <dkseotn123@gmail.com>
2023-01-08 00:08:59 +0900

    feat: 연구실 문제 추가

---
+++ Graph/Laboratory/Laboratory/Laboratory/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  Laboratory
+//
+//  Created by Kay on 2023/01/08.
+//
+
+/*
+ 14502
+ 연구소
+ */
+// 2nd week fast campus
+
+/*
+ 지금 당장 생각 나는것
+ 
+ 1. 시간복잡도 계산 64C3 * (64 + 64)
+ 2. 벽 3개 세우기
+ 3. bfs + queue를 이용한 최단거리들 중 max 값
+ 4. 방문 배열 초기화
+ 5. 로직은 내일
+ */
+import Foundation
+
+
