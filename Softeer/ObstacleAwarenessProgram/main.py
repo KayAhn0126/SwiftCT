2abbd9bbd47d8f9d8b33a7100843c07757c3b442
KayAhn0126 <dkseotn123@gmail.com>
2023-02-01 01:18:04 +0900

    modify: 장애물 인식 프로그램 문제 해결 업데이트

--- Softeer/ObstacleAwarenessProgram/main.py
+++ Softeer/ObstacleAwarenessProgram/main.py
@@ -1,12 +1,10 @@
 import sys
-from queue import Queue
 
 userInput = int(sys.stdin.readline())
 
-arr = [list(map(int, sys.stdin.readline().split())) for _ in range(userInput)]
 arr = []
-for i in userInput:
-    arr.append(list(map(int, )))
+for i in range(userInput):
+    arr.append(list(map(int, input())))
 
 visited = [[0 for _ in range(userInput)] for _ in range(userInput)]
 
@@ -15,41 +13,41 @@ resultList = []
 dy = [-1,0,1,0]
 dx = [0,1,0,-1]
 
-for i in arr:
-    print(i)
 
-for i in arr:
-    print(i)
+for i in range(userInput):
+    print(arr[i])
 
 
-def bfs(y,x):
-    current = 0
-    queue = Queue()
+
+
+global highest
+highest = 1
+
+def dfs(y,x):
+    global highest
     visited[y][x] = 1
-    while queue.empty() == False :
-        currentYX = queue.get()
-        for i in range(4):
-            ny = currentYX[0] + dy[i]
-            nx = currentYX[1] + dx[i]
-            if arr[ny][nx] == 0 or visited[ny][nx] != 0:
-                continue
-            visited[ny][nx] = visited[y][x] + 1
-            current = visited[ny][nx]
-            queue.put([ny,nx])
-    resultList.append(current)
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if ny < 0 or nx < 0 or ny >= userInput or nx >= userInput : continue
+        if arr[ny][nx] == 0 or visited[ny][nx] != 0:
+            continue
+        highest += 1
+        dfs(ny,nx)
 
 for i in range(userInput):
     for j in range(userInput):
         if arr[i][j] == 0 or visited[i][j] != 0 :
             continue
-        bfs(i,j)
-        
-        
-print(resultList.count)
+        highest = 1
+        dfs(i,j)
+        resultList.append(highest)
+
+print(len(resultList))
 resultList.sort()
 for i in resultList:
     print(i)
-        
+
 
 
 
