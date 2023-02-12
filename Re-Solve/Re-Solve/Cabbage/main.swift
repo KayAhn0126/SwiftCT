284705a51b911c49bca38e27a7b75234f0ed9640
KayAhn0126 <dkseotn123@gmail.com>
2023-02-12 23:52:04 +0900

    add: 유기농 배추 문제 다시 풀기 추가
    문제 접근만 추가

---
+++ Re-Solve/Re-Solve/Cabbage/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Cabbage
+//
+//  Created by Kay on 2023/02/12.
+//
+
+/*
+ 1012
+ 유기농 배추
+ */
+
+/*
+ connected component 갯수 구하기
+ 맵 크기 -> 최대 50 * 50
+ 배추의 위치가 인접 리스트로 주어진다.
+ */
+import Foundation
+
+
+
