3d48c41419956cc4d4e1561e202116582961df1f
KayAhn0126 <dkseotn123@gmail.com>
2023-03-04 13:02:23 +0900

    add: RGB 거리 문제 해결 및 추가

--- DP/RGBStreet/RGBStreet/RGBStreet/main.swift
+++ DP/RGBStreet/RGBStreet/RGBStreet/main.swift
@@ -12,5 +12,26 @@
 
 import Foundation
 
+let streets = Int(readLine()!)!
+var streetsArr = [[Int]](repeating: [Int](), count: streets)
 
+for i in 0..<streets {
+    streetsArr[i].append(contentsOf: readLine()!.split(separator: " ").map { Int(String($0))! })
+}
+
+var dp = [[Int]](repeating: [Int](repeating: 0, count: 3), count: streets)
+var prevStreet = streetsArr[0]
+dp[0][0] = prevStreet[0]
+dp[0][1] = prevStreet[1]
+dp[0][2] = prevStreet[2]
+
+for i in 1..<streets {
+    // r
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + streetsArr[i][0]
+    // g
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + streetsArr[i][1]
+    // b
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + streetsArr[i][2]
+}
+print(min(dp[streets-1][0], dp[streets-1][1], dp[streets-1][2]))
 
