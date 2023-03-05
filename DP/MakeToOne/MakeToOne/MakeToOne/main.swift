e4d55fe3b74890c69b90838e9eeafe6b1c682a70
KayAhn0126 <dkseotn123@gmail.com>
2023-03-05 21:17:07 +0900

    add: 1로 만들기 문제 해결 및 추가

---
+++ DP/MakeToOne/MakeToOne/MakeToOne/main.swift
@@ -0,0 +1,41 @@
+//
+//  main.swift
+//  MakeToOne
+//
+//  Created by Kay on 2023/03/05.
+//
+
+/*
+ 1463
+ 1로 만들기
+ */
+
+/*
+ 정수 X에 사용할 수 있는 연산은 다음과 같이 세 가지 이다.
+
+ 1. X가 3으로 나누어 떨어지면, 3으로 나눈다.
+ 2. X가 2로 나누어 떨어지면, 2로 나눈다.
+ 3. 1을 뺀다.
+ 정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 한다. 연산을 사용하는 횟수의 최솟값을 출력하시오.
+ */
+
+import Foundation
+
+let userInput = Int(readLine()!)!
+var dp = [Int](repeating: 0, count: 1000001)
+
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
+
+for i in 4..<1000001 {
+    dp[i] = dp[i-1] + 1
+    if i % 2 == 0 {
+        dp[i] = min(dp[i / 2] + 1 , dp[i])
+    }
+    if i % 3 == 0 {
+        dp[i] = min(dp[i / 3] + 1 , dp[i])
+    }
+}
+
+print(dp[userInput])
