d3df61fc8a34a5c2b7a8bd91eff270b3e52671df
KayAhn0126 <dkseotn123@gmail.com>
2023-03-07 19:11:32 +0900

    update: 컴백홈 문제 해결 및 업데이트

--- Graph/ComebackHome/ComebackHome/ComebackHome/main.swift
+++ Graph/ComebackHome/ComebackHome/ComebackHome/main.swift
@@ -5,6 +5,10 @@
 //  Created by Kay on 2023/01/11.
 //
 
+/*
+ 1189
+ 컴백홈
+ */
 import Foundation
 
 let dy = [-1,0,1,0]
@@ -15,8 +19,13 @@ let R = RCK[0]
 let C = RCK[1]
 let K = RCK[2]
 
+/*
+ 재귀를 이용한 백트래킹
+ 5x5에서 각 위치에서 전체 탐색 -> 시간복잡도는 5 * 5 * 25 = 625
+ */
+
 var map = [[Character]](repeating: [Character](), count: R)
-var visited = [[Bool]](repeating: [Bool](repeating: false, count: C), count: R)
+var visited = [[Int]](repeating: [Int](repeating: 0, count: C), count: R)
 
 
 for i in 0..<R {
@@ -24,34 +33,23 @@ for i in 0..<R {
 }
 
 
-var dfsCount = 0
-var result = 0
-
-func clearVisited() {
-    for i in 0..<R {
-        for j in 0..<C {
-            visited[i][j] = false
+func search(_ y: Int, _ x: Int) -> Int {
+    if y == 0 && x == C-1 {
+        if K == visited[y][x] {
+            return 1
         }
+        return 0
     }
-}
-
-func dfs(_ y: Int,  _ x: Int) {
-    dfsCount += 1
-    visited[y][x] = true
-//    if y == R-1 && x == C-1 && K == dfsCount {
-//        result += 1
-//        dfsCount = 0
-//    }
+    var result = 0
     for i in 0..<4 {
         let ny = y + dy[i]
         let nx = x + dx[i]
-        if ny < 0 || nx < 0 || ny >= R || nx >= C || map[ny][nx] == "T" || visited[ny][nx] == true { continue }
-        dfs(ny,nx)
-        visited[ny][nx] = false
+        if ny < 0 || nx < 0 || ny >= R || nx >= C || visited[ny][nx] > 0 || map[ny][nx] == "T" { continue }
+        visited[ny][nx] = visited[y][x] + 1
+        result += search(ny,nx)
+        visited[ny][nx] = 0
     }
+    return result
 }
-
-
-clearVisited()
-dfs(0,0)
-print(result)
+visited[R-1][0] = 1
+print(search(R-1, 0))
