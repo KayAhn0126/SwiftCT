dfbe4393e112da966d06774502f00e7903c6ecf4
KayAhn0126 <dkseotn123@gmail.com>
2023-03-08 23:31:05 +0900

    update: 주난의 난 문제 해결 업데이트

--- Graph/Junan/Junan/Junan/main.swift
+++ Graph/Junan/Junan/Junan/main.swift
@@ -33,29 +33,33 @@ var visited = [[Int]](repeating: [Int](repeating: 0, count: M), count: N)
 let dy = [-1,0,1,0]
 let dx = [0,1,0,-1]
 
-var bfsQueue = [(Int,Int)](), idx = 0
+var zeroBfsQueue = [(Int,Int)](), zeroIndex = 0
 visited[junanLocationY][junanLocationX] = 1
-bfsQueue.append((junanLocationY, junanLocationX))
+zeroBfsQueue.append((junanLocationY, junanLocationX))
 var result = 0
-while idx < bfsQueue.count {
-    let currentLocation = bfsQueue[idx]; idx += 1
-    let currentLocationY = currentLocation.0
-    let currentLocationX = currentLocation.1
-    if currentLocationY == theftLocationY && currentLocationX == theftLocationX {
-        result = visited[theftLocationY][theftLocationX]
-        break
-    }
-    for i in 0..<4 {
-        let ny = currentLocationY + dy[i]
-        let nx = currentLocationX + dx[i]
-        if ny < 0 || nx < 0 || ny >= N || nx >= M { continue }
-        if visited[ny][nx] > 0 { continue }
-        if adjMatrix[ny][nx] == "0" || adjMatrix[ny][nx] == "#" {
-            visited[ny][nx] = visited[currentLocationY][currentLocationX]
-        } else if adjMatrix[ny][nx] == "1" {
-            visited[ny][nx] = visited[currentLocationY][currentLocationX] + 1
+
+while adjMatrix[theftLocationY][theftLocationX] != "0" {
+    result += 1
+    var oneBfsQueue = [(Int,Int)]()
+    while zeroIndex < zeroBfsQueue.count {
+        let currentLocation = zeroBfsQueue[zeroIndex]; zeroIndex += 1
+        let currentLocationY = currentLocation.0
+        let currentLocationX = currentLocation.1
+        for i in 0..<4 {
+            let ny = currentLocationY + dy[i]
+            let nx = currentLocationX + dx[i]
+            if ny < 0 || nx < 0 || ny >= N || nx >= M { continue }
+            if visited[ny][nx] != 0 { continue }
+            visited[ny][nx] = result
+            if adjMatrix[ny][nx] != "0" {
+                adjMatrix[ny][nx] = "0"
+                oneBfsQueue.append((ny,nx))
+            } else {
+                zeroBfsQueue.append((ny,nx))
+            }
         }
-        bfsQueue.append((ny,nx))
     }
+    zeroBfsQueue = oneBfsQueue
+    zeroIndex = 0
 }
-print(result)
+print(visited[theftLocationY][theftLocationX])
