5d5a4e76ee984298decb6ad6bc13e04a4bd17fac
KayAhn0126 <dkseotn123@gmail.com>
2023-03-27 18:02:28 +0900

    update: 알파벳 문제 해결 및 업데이트

--- Graph/Alphabet/Alphabet/Alphabet/main.swift
+++ Graph/Alphabet/Alphabet/Alphabet/main.swift
@@ -11,11 +11,20 @@
  */
 
 /*
+ //MARK: 처음 생각했던 방식 -> 시간초과
  다음 가려는 곳이 계수 정렬 값이 1이라면 continue.
  queue에 아무것도 남지 않으면 종료
  매번 max값 갱신
  백트래킹 할 때 계수 정렬 배열도 같이 해주기.
- 
+ */
+
+/*
+ //MARK: 통과한 방식 -> 비트마스킹
+ 계수 정렬 배열을 비트마스킹으로 바꾸기 + 기존 방문 배열(visited) 없애기
+ 계수 정렬 배열을 방문 배열로 두어도 괜찮기 때문에 기존 방문 배열을 없앴다.
+    - 무작정 쓰지 말자
+ 알파벳이 단 한번씩만 나와야 하므로 만약 A를 만났던 상태에서 다시 A를 만나면 continue가 된다.
+ 자세한건 README.md에 정리
  */
 
 import Foundation
@@ -33,28 +42,22 @@ for i in 0..<R {
 let dy = [-1,0,1,0]
 let dx = [0,1,0,-1]
 
-var countingArr = [Bool](repeating: false, count: 26)
-var visited = [[Int]](repeating: [Int](repeating: 0, count: C), count: R)
-
-
-visited[0][0] = 1
-countingArr[adjMatrix[0][0]] = true
+var visitedNumber: Int = (1 << 26)
+visitedNumber |= (1 << adjMatrix[0][0])
 
 var result = 0
-func search(_ y: Int, _ x: Int) {
+func search(_ y: Int, _ x: Int, _ depth: Int) {
+    result = depth > result ? depth : result
     for i in 0..<4 {
         let ny = y + dy[i]
         let nx = x + dx[i]
         if ny < 0 || nx < 0 || ny >= R || nx >= C { continue }
-        if visited[ny][nx] > 0 { continue }
-        if countingArr[adjMatrix[ny][nx]] == true { continue }
-        visited[ny][nx] = visited[y][x] + 1
-        result = visited[ny][nx] > result ? visited[ny][nx] : result
-        countingArr[adjMatrix[ny][nx]] = true
-        search(ny,nx)
-        visited[ny][nx] = 0
-        countingArr[adjMatrix[ny][nx]] = false
+        let idx = 1 << adjMatrix[ny][nx]
+        if visitedNumber & (1 << idx) != 0 { continue }
+        visitedNumber |= (1 << idx)
+        search(ny, nx, depth + 1)
+        visitedNumber &= ~(1 << idx)
     }
 }
-search(0,0)
+search(0,0,1)
 print(result)
