20426c2c1cb185777237e9991542b87cde7149ba
KayAhn0126 <dkseotn123@gmail.com>
2023-04-11 20:25:46 +0900

    update: 한국이 그리울땐 서버에 접속하지 문제 다시 풀기 추가

--- Re-Solve/Re-Solve/WhenIMissKoreaConnectToServer/main.swift
+++ Re-Solve/Re-Solve/WhenIMissKoreaConnectToServer/main.swift
@@ -1,35 +1,16 @@
-//
-//  main.swift
-//  WhenIMissKoreaConnectToServer
-//
-//  Created by Kay on 2023/02/02.
-//
-
-/*
- 9996
- 한국이 그리울 땐 서버에 접속하지
- */
-
 import Foundation
 
-func checkPattern(_ testString: String) {
-    if testString.hasPrefix(prefixString) && testString.hasSuffix(suffixString) && testString.count >= standard {
+let num = Int(readLine()!)!
+
+let lineArr = readLine()!.split(separator: "*")
+let pre = lineArr[0]
+let suf = lineArr[1]
+
+for i in 0..<num {
+    let temp = readLine()!
+    if temp.hasPrefix(pre) && temp.hasSuffix(suf) && temp.count >= (pre.count + suf.count) {
         print("DA")
     } else {
         print("NE")
     }
 }
-
-let userInput = Int(readLine()!)!
-let patternString = readLine()!
-
-let starIndex = patternString.firstIndex(of: "*")!
-let prefixString = String(patternString[patternString.startIndex..<starIndex])
-let suffixString = String(patternString[patternString.index(after: starIndex)..<patternString.endIndex])
-let standard = prefixString.count + suffixString.count
-
-for _ in 0..<userInput {
-    let testString = readLine()!
-    checkPattern(testString)
-}
-
