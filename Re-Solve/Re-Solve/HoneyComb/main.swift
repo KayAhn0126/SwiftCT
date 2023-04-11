1d473f0e10ebddb344d9caf0899f32148f30d780
KayAhn0126 <dkseotn123@gmail.com>
2023-04-11 23:30:08 +0900

    update: 벌집 문제 다시 풀기 추가

--- Re-Solve/Re-Solve/HoneyComb/main.swift
+++ Re-Solve/Re-Solve/HoneyComb/main.swift
@@ -1,30 +1,16 @@
-//
-//  main.swift
-//  HoneyComb
-//
-//  Created by Kay on 2023/03/18.
-//
-
-/*
- 2292
- 벌집 다시풀기 추가
- */
-
-/*
- 1     -> 1
- 2 ~ 7 -> 2
- 8 ~ 19 -> 3
- 20 ~ 37 -> 4
- 
- */
 import Foundation
 
-let number = Int(readLine()!)!
+let target = Int(readLine()!)!
 
-var count = 1 // layer
-var temp = 1 // 숫자
-while number > temp {
-    temp += count * 6
-    count += 1
+var base = 1
+var layer = 1
+
+if target == 1 {
+    print(1)
+} else {
+    while base < target {
+        base += (layer * 6)
+        layer += 1
+    }
+    print(layer)
 }
-print(count)
