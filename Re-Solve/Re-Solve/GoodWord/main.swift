020447bfbb141872b4096a5bf48d5f48cfe269c5
KayAhn0126 <dkseotn123@gmail.com>
2023-04-11 23:15:46 +0900

    update: 좋은단어 문제 다시 풀기 추가

--- Re-Solve/Re-Solve/GoodWord/main.swift
+++ Re-Solve/Re-Solve/GoodWord/main.swift
@@ -1,69 +1,39 @@
-//
-//  main.swift
-//  GoodWord
-//
-//  Created by Kay on 2023/02/02.
-//
-
-/*
- 3986
- 좋은 단어
- */
 import Foundation
 
-extension String {
-    subscript(idx: Int) -> Character? {
-        guard (0..<count).contains(idx) else { return nil }
-        let target = index(startIndex, offsetBy: idx)
-        return self[target]
-    }
-}
-
 struct Stack<T> {
-    var stack: [T] = []
-    
+    var element: [T] = []
     var count: Int {
-        return stack.count
+        return element.count
     }
-    
     var isEmpty: Bool {
-        return stack.isEmpty
+        return element.isEmpty
     }
-    
     var top: T? {
-        return stack.last
+        return element.last
     }
-    
-    mutating func push(_ element: T) {
-        stack.append(element)
+    mutating func push(_ ele: T) {
+        element.append(ele)
     }
-    
     mutating func pop() -> T? {
-        return stack.popLast()
+        element.popLast()
     }
 }
 
-let tryCount = Int(readLine()!)!
-if 1 <= tryCount && tryCount <= 100 {
-    var result = 0
-    for _ in 0..<tryCount {
-        var myStack = Stack<Character>()
-        let testString = readLine()!
-        let testStringArr = [Character](testString)
-        for i in 0..<testStringArr.count {
-            if myStack.isEmpty {
-                myStack.push(testStringArr[i])
-            } else {
-                if myStack.top! == testStringArr[i] {
-                    myStack.pop()
-                } else {
-                    myStack.push(testStringArr[i])
-                }
-            }
-        }
-        if myStack.isEmpty {
-            result += 1
+let num = Int(readLine()!)!
+var result = 0
+for i in 0..<num {
+    let temp = readLine()!
+    var stack = Stack<Character>()
+    var lastChar: Character = " "
+    for i in temp {
+        if stack.isEmpty == true || (stack.isEmpty == false && stack.top! != i) {
+            stack.push(i)
+        } else if stack.isEmpty == false && stack.top! == i {
+            stack.pop()!
         }
     }
-    print(result)
+    if stack.isEmpty == true {
+        result += 1
+    }
 }
+print(result)
