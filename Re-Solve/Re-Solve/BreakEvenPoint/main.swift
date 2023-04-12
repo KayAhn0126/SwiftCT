cd8f06043ade0d108a46eb8d341d05c29fc07e5f
KayAhn0126 <dkseotn123@gmail.com>
2023-04-12 18:31:35 +0900

    update: 손익분기점 문제 다시 풀기 추가

--- Re-Solve/Re-Solve/BreakEvenPoint/main.swift
+++ Re-Solve/Re-Solve/BreakEvenPoint/main.swift
@@ -1,18 +1,3 @@
-//
-//  main.swift
-//  BreakEvenPoint
-//
-//  Created by Kay on 2023/02/03.
-//
-
-/*
- 1712
- 손익분기점
- */
-
-// 고정비용 A
-// 노트북 1대 만들때 마다 필요한 금액 (재료비 + 인건비 가변 비용) B
-// 노트북 가격 C
 import Foundation
 
 let ABC = readLine()!.split(separator: " ").map { Int(String($0))! }
@@ -23,5 +8,5 @@ let C = ABC[2]
 if B >= C {
     print(-1)
 } else {
-    print((A / (C - B)) + 1)
+    print(A/(C-B)+1)
 }
