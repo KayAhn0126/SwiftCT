e4f5f360440c231b820c1d2645004528849c620d
KayAhn0126 <dkseotn123@gmail.com>
2023-04-12 23:51:39 +0900

    update: 곱셈 문제 다시 풀기 추가

--- Re-Solve/Re-Solve/Multiply/main.swift
+++ Re-Solve/Re-Solve/Multiply/main.swift
@@ -1,41 +1,22 @@
-//
-//  main.swift
-//  Multiply
-//
-//  Created by Kay on 2023/02/02.
-//
-
-/*
- 1629
- 곱셈
- */
-
-// 자연수 A를 B번 곱한 수를 알고 싶다. 단 구하려는 수가 매우 커질 수 있으므로 이를 C로 나눈 나머지를 구하는 프로그램을 작성하시오.
-
 import Foundation
 
-let userInput = readLine()!.split(separator: " ").map { Int(String($0))! }
-let A = userInput[0]
-let B = userInput[1]
-let C = userInput[2]
+let ABC = readLine()!.split(separator: " ").map { Int(String($0))! }
+let A = ABC[0]
+let B = ABC[1]
+let C = ABC[2]
 
-// 정수론 나머지
-// A % B = (A % B) % B
-// (A + B) % C = ((A % C) + (B % C)) % C
-// 재귀함수는 기저사례 꼭 생각하자..
+// (a * b) % c = ((a % c) * (b % c)) % c
 
-var result = 1
-func calculate(_ a: Int, _ b: Int) -> Int {
+func go(_ b: Int) -> Int {
     if b == 1 {
-        return a % C
+        return A % C
     }
-    result = calculate(a, b/2)
+    var result = go(b/2)
     result = (result * result) % C
-    
     if b % 2 != 0 {
-        result = (result * a) % C
+        result = (result * A) % C
     }
     return result
 }
 
-print(calculate(A,B))
+print(go(B))
