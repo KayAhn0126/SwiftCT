fed8d992e15648a67ce352da6569a0e8c2924937
KayAhn0126 <dkseotn123@gmail.com>
2023-04-13 17:21:15 +0900

    update: NBA농구 문제 다시 풀기 추가

--- Re-Solve/Re-Solve/NBABasketBall/main.swift
+++ Re-Solve/Re-Solve/NBABasketBall/main.swift
@@ -1,67 +1,74 @@
-//
-//  main.swift
-//  NBABasketBall
-//
-//  Created by Kay on 2023/02/03.
-//
-
 import Foundation
 
-let goals = Int(readLine()!)!
+// 1 - 팀, 시간 입력 받기
+// 2 - 현재 이기고 있는 팀에 시간 더해주기
+// 3 - 현재 시간을 이전 시간 변수에 넣어주기 (2번 작업을 위함)
+// 4 - 마지막에 48분에서 한번 빼주기
 
-var teamAScore = 0 // 1
-var teamBScore = 0 // 2
+// 시간은 주어진 값들 중에서 최소단위로!
+
+let num = Int(readLine()!)!
+
+var teamAScore = 0
+var teamBScore = 0
 
 var teamATime = 0
 var teamBTime = 0
 
-func changeToSec(_ element: String) -> Int {
-    let arr = element.split(separator: ":").map { Int(String($0))! }
-    let min = arr[0] * 60
-    let sec = arr[1]
-    return min + sec
+var prevTime = ""
+
+//MARK: 문자열 시간을 초로 바꿔주는 함수 - String -> Int
+func toSecond(_ time: String) -> Int {
+    let newTime = time.split(separator: ":").map { Int(String($0))! }
+    let min = newTime[0] * 60
+    let second = newTime[1]
+    return min + second
 }
 
-func changeToMin(_ element: Int) -> String {
-    let min = element / 60
-    let sec = element % 60
-    if min < 10 {
-        if sec < 10 {
-            return "0\(min):0\(sec)"
-        } else {
-            return "0\(min):\(sec)"
-        }
-    } else {
-        if sec < 10 {
-            return "\(min):0\(sec)"
-        } else {
-            return "\(min):\(sec)"
-        }
+//MARK: 시간 계산 함수 - 단위(초)
+func calculateTime(_ from: Int, _ to: Int) -> Int {
+    return from - to
+}
+
+//MARK: 숫자를 시간으로 바꿔주는 함수 - Int -> String
+func toTimeToString(_ num: Int) -> String {
+    let newNum = num
+    let min = newNum / 60
+    let sec = newNum % 60
+    var result = "00:00"
+    if min < 10 && sec < 10 {
+        result = "0\(min):0\(sec)"
+    } else if min < 10 && sec >= 10 {
+        result = "0\(min):\(sec)"
+    } else if min >= 10 && sec < 10 {
+        result = "\(min):0\(sec)"
+    } else if min >= 10 && sec >= 10{
+        result = "\(min):\(sec)"
     }
+    return result
 }
 
-var previous = ""
-for _ in 0..<goals {
-    var teamAndTime = readLine()!.split(separator: " ")
-    let team = Int(String(teamAndTime[0]))
-    let time = String(teamAndTime[1])
+for _ in 0..<num {
+    let teamTime = readLine()!.split(separator: " ").map { String($0) }
+    let team = teamTime[0]
+    let time = teamTime[1]
+    
     if teamAScore > teamBScore {
-        teamATime += changeToSec(time) - changeToSec(previous)
-    } else if teamBScore > teamAScore {
-        teamBTime += changeToSec(time) - changeToSec(previous)
+        teamATime += calculateTime(toSecond(time), toSecond(prevTime))
+    } else if teamAScore < teamBScore {
+        teamBTime += calculateTime(toSecond(time), toSecond(prevTime))
     }
-    if team == 1 {
+    if team == "1" {
         teamAScore += 1
     } else {
         teamBScore += 1
     }
-    previous = time
+    prevTime = time
 }
 if teamAScore > teamBScore {
-    teamATime += changeToSec("48:00") - changeToSec(previous)
-} else if teamBScore > teamAScore {
-    teamBTime += changeToSec("48:00") - changeToSec(previous)
+    teamATime += calculateTime(toSecond("48:00"), toSecond(prevTime))
+} else if teamAScore < teamBScore {
+    teamBTime += calculateTime(toSecond("48:00"), toSecond(prevTime))
 }
-
-print(changeToMin(teamATime))
-print(changeToMin(teamBTime))
+print(toTimeToString(teamATime))
+print(toTimeToString(teamBTime))
