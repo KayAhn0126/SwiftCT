7faf0a64d03febb13ca4e0827980b754f7200ed9
KayAhn0126 <dkseotn123@gmail.com>
2023-04-14 14:09:52 +0900

    update: 로봇청소기 문제 다시 풀기 추가

--- Re-Solve/Re-Solve/RobotCleaner/main.swift
+++ Re-Solve/Re-Solve/RobotCleaner/main.swift
@@ -1,74 +1,52 @@
-//
-//  main.swift
-//  RobotCleaner
-//
-//  Created by Kay on 2023/03/18.
-//
-
-/*
- 14503
- 로봇 청소기
- */
-
-/*
- 왼쪽으로 방향전환 -> (current + 3) % 4
- 뒤로 방향 전환 -> (current + 2) % 4
- */
-
 import Foundation
 
 let NM = readLine()!.split(separator: " ").map { Int(String($0))! }
 let N = NM[0]
 let M = NM[1]
+let currentLocationAndDirection = readLine()!.split(separator: " ").map { Int(String($0))! }
 
-let currentLocation = readLine()!.split(separator: " ").map { Int(String($0))! }
-var currentY = currentLocation[0]
-var currentX = currentLocation[1]
-var currentD = currentLocation[2] // Direction
+var cLocY = currentLocationAndDirection[0]
+var cLocX = currentLocationAndDirection[1]
+var cDir = currentLocationAndDirection[2]
 
 var adjMatrix = [[Int]](repeating: [Int](), count: N)
+
+let dy = [-1,0,1,0]
+let dx = [0,1,0,-1]
 
 for i in 0..<N {
     adjMatrix[i].append(contentsOf: readLine()!.split(separator: " ").map { Int(String($0))! })
 }
 
-// 0 청소 안된 칸
-// 1은 벽
-// 2는 청소한 칸
-
-let dy = [-1,0,1,0]
-let dx = [0,1,0,-1]
-var turnCount = 0
 var result = 0
+var queue = [(Int, Int)](), idx = 0
+queue.append((cLocY, cLocX))
 
-while true {
-    if adjMatrix[currentY][currentX] == 0 {
-        adjMatrix[currentY][currentX] = 2
+while idx < queue.count {
+    let currtLocation = queue[idx]; idx += 1
+    let curY = currtLocation.0
+    let curX = currtLocation.1
+    if adjMatrix[curY][curX] == 0 {
+        adjMatrix[curY][curX] = 2
         result += 1
     }
-    turnCount = 0
+    var count = 0
     for i in 0..<4 {
-        let nextDirection = (currentD + 3 - i) % 4
-        let nextY = currentY + dy[nextDirection]
-        let nextX = currentX + dx[nextDirection]
-        if nextY < 0 || nextX < 0 || nextY >= N || nextX >= M { continue }
-        if adjMatrix[nextY][nextX] == 0 {
-            turnCount += 1
-            currentY = nextY
-            currentX = nextX
-            currentD = nextDirection
-            break
-        }
+        let nd = (cDir + 3 - i) % 4
+        let ny = curY + dy[nd]
+        let nx = curX + dx[nd]
+        if ny < 0 || nx < 0 || ny >= N || nx >= M || adjMatrix[ny][nx] != 0 { continue }
+        count += 1
+        cDir = nd
+        queue.append((ny,nx))
+        break
     }
-    if turnCount == 0 {
-        let backDirection = (currentD + 2) % 4
-        let backY = currentY + dy[backDirection]
-        let backX = currentX + dx[backDirection]
-        if backY < 0 || backX < 0 || backY >= N || backX >= M || adjMatrix[backY][backX] == 1 {
-            print(result)
-            break
-        }
-        currentY = backY
-        currentX = backX
+    if count == 0 {
+        let backDir = (cDir + 2) % 4
+        let backY = curY + dy[backDir]
+        let backX = curX + dx[backDir]
+        if backY < 0 || backX < 0 || backY >= N || backX >= M || adjMatrix[backY][backX] == 1 { break }
+        queue.append((backY, backX))
     }
 }
+print(result)
