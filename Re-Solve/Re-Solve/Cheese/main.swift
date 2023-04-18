06658196b5a20c7b5f22133bb34dde452df80b82
KayAhn0126 <dkseotn123@gmail.com>
2023-04-18 22:10:05 +0900

    update: 치즈 문제 다시 풀기 추가

--- Re-Solve/Re-Solve/Cheese/main.swift
+++ Re-Solve/Re-Solve/Cheese/main.swift
@@ -1,15 +1,3 @@
-//
-//  main.swift
-//  Cheese
-//
-//  Created by Kay on 2023/02/22.
-//
-
-/*
- 2636
- 치즈
- */
-
 import Foundation
 
 let NM = readLine()!.split(separator: " ").map { Int(String($0))! }
@@ -17,70 +5,65 @@ let N = NM[0]
 let M = NM[1]
 
 var adjMatrix = [[Int]](repeating: [Int](), count: N)
-var visited = [[Int]](repeating: [Int](repeating: 0, count: M), count: N)
+
 for i in 0..<N {
     adjMatrix[i].append(contentsOf: readLine()!.split(separator: " ").map { Int(String($0))! })
 }
 
-var meltingList: [(Int,Int)] = []
-var cheeseCount = 0
-var totalTime = 0
-
 let dy = [-1,0,1,0]
 let dx = [0,1,0,-1]
 
-//MARK: 치즈 녹이기
-// -> adjMatrix에서 1이고 visited 하지 않았다면 치즈를 녹일 배열에 해당 치즈의 위치를 넣는다.
-func findingLocationToMeltDFS(_ y: Int, _ x: Int) {
-    visited[y][x] = 1
-    if adjMatrix[y][x] == 1 {
-        meltingList.append((y,x))
-        return
-    }
+//MARK: 녹일 치즈 찾기
+var list: [(Int,Int)] = []
+func findMeltingPoints(_ y: Int, _ x: Int, _ visited: inout [[Bool]]) {
     for i in 0..<4 {
         let ny = y + dy[i]
         let nx = x + dx[i]
         if ny < 0 || nx < 0 || ny >= N || nx >= M { continue }
-        if visited[ny][nx] == 1 { continue }
-        findingLocationToMeltDFS(ny, nx)
-    }
-}
-
-func visitedClear() {
-    for i in 0..<N {
-        for j in 0..<M {
-            visited[i][j] = 0
+        if visited[ny][nx] == true { continue }
+        if adjMatrix[ny][nx] == 1 {
+            list.append((ny,nx))
+            continue
         }
+        visited[ny][nx] = true
+        findMeltingPoints(ny,nx,&visited)
     }
-}
-//MARK: 실제로 치즈 녹이기
-func melting() {
-    meltingList.enumerated().forEach {
-        adjMatrix[$0.element.0][$0.element.1] = 0
-    }
-    meltingList.removeAll()
 }
 
-//MARK: 치즈 갯수 구하기
-func getNumberOfCheese() -> Int {
-    var count = 0
+//MARK: 실제 치즈 녹이기
+func meltingCheese() {
+    for i in list {
+        adjMatrix[i.0][i.1] = 0
+    }
+    list.removeAll()
+}
+
+//MARK: 치즈 조각 갯수 세기
+func getCount() -> Int {
+    var result = 0
     for i in 0..<N {
         for j in 0..<M {
             if adjMatrix[i][j] == 1 {
-                count += 1
+                result += 1
             }
         }
     }
-    return count
+    return result
 }
 
-while getNumberOfCheese() != 0 {
-    visitedClear()
-    cheeseCount = getNumberOfCheese()
-    findingLocationToMeltDFS(0,0)
-    melting()
-    totalTime += 1
-    
+var time = 0
+var last = 0
+while true {
+    let number = getCount()
+    if number == 0 {
+        print(time)
+        print(last)
+        break
+    }
+    last = number
+    time += 1
+    var visited = [[Bool]](repeating: [Bool](repeating: false, count: M), count: N)
+    visited[0][0] = true
+    findMeltingPoints(0, 0, &visited)
+    meltingCheese()
 }
-print(totalTime)
-print(cheeseCount)
