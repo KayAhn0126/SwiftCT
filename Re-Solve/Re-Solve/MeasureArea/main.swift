a63b6486b9713b17e98ce4632071fe7554b3946d
KayAhn0126 <dkseotn123@gmail.com>
2023-04-18 12:15:56 +0900

    update: 영역 구하기 문제 다시 풀기 추가

--- Re-Solve/Re-Solve/MeasureArea/main.swift
+++ Re-Solve/Re-Solve/MeasureArea/main.swift
@@ -1,96 +1,61 @@
-//
-//  main.swift
-//  MeasureArea
-//
-//  Created by Kay on 2023/02/16.
-//
-
-/*
- 2583
- 영역 구하기
- */
-
 import Foundation
 
-struct Queue<T> {
-    var enqueueList: [T] = []
-    var dequeueList: [T] = []
-    
-    var count: Int {
-        return enqueueList.count + dequeueList.count
-    }
-    
-    var isEmpty: Bool {
-        return enqueueList.isEmpty && dequeueList.isEmpty
-    }
-    
-    mutating func enqueue(_ element: T) {
-        enqueueList.append(element)
-    }
-    
-    mutating func dequeue() -> T? {
-        if dequeueList.isEmpty {
-            dequeueList = enqueueList.reversed()
-            enqueueList.removeAll()
-        }
-        return dequeueList.popLast()
-    }
-}
-
 let MNK = readLine()!.split(separator: " ").map { Int(String($0))! }
-
 let N = MNK[0]
 let M = MNK[1]
 let K = MNK[2]
 
-var matrix = [[Int]](repeating: [Int](repeating: 1, count: M), count: N)
-var visited = [[Int]](repeating: [Int](repeating: 0, count: M), count: N)
+var adjMatrix = [[Int]](repeating: [Int](repeating: 0, count: M), count: N)
+var visited = [[Bool]](repeating: [Bool](repeating: false, count: M), count: N)
 
-let dy = [-1,0,1,0]
-let dx = [0,1,0,-1]
-
-
+// 주어진대로 색칠하기
+// dfs로 범위 구하기 -> arr에 넣기
+// CC 갯수 구하기
 
 for i in 0..<K {
-    let location = readLine()!.split(separator: " ").map { Int(String($0))! }
-    let x1 = location[0]
-    let y1 = location[1]
-    let x2 = location[2]
-    let y2 = location[3]
-    for row in y1..<y2 {
-        for col in x1..<x2 {
-            matrix[row][col] = 0
+    let locations = readLine()!.split(separator: " ").map { Int(String($0))! }
+    let fromY = locations[1]
+    let fromX = locations[0]
+    let toY = locations[3]
+    let toX = locations[2]
+    for j in fromY..<toY {
+        for k in fromX..<toX {
+            adjMatrix[j][k] = 1
         }
     }
 }
 
-var count = 0
-func dfs(_ y: Int, _ x: Int) {
-    count += 1
-    visited[y][x] = 1
+let dy = [-1,0,1,0]
+let dx = [0,1,0,-1]
+
+func dfs(_ y: Int, _ x: Int, _ areaCounter: inout Int) {
+    areaCounter += 1
     for i in 0..<4 {
         let ny = y + dy[i]
         let nx = x + dx[i]
         if ny < 0 || nx < 0 || ny >= N || nx >= M { continue }
-        if visited[ny][nx] > 0 || matrix[ny][nx] == 0 { continue }
-        dfs(ny,nx)
+        if visited[ny][nx] == true { continue }
+        if adjMatrix[ny][nx] != 0 { continue }
+        visited[ny][nx] = true
+        dfs(ny, nx, &areaCounter)
     }
 }
 
-var totalCount = 0
-var result: [Int] = []
+var resultArr = [Int]()
+var totalCounter = 0
 for i in 0..<N {
     for j in 0..<M {
-        if visited[i][j] == 1 || matrix[i][j] == 0 { continue }
-        count = 0
-        totalCount += 1
-        dfs(i,j)
-        result.append(count)
+        if visited[i][j] == true { continue }
+        if adjMatrix[i][j] == 1 { continue }
+        totalCounter += 1
+        visited[i][j] = true
+        var area = 0
+        dfs(i, j, &area)
+        resultArr.append(area)
     }
 }
 
-print(totalCount)
-result.sorted(by: <).enumerated().forEach {
-    print($0.element, terminator: " ")
+print(totalCounter)
+resultArr.sorted(by: < ).forEach {
+    print($0)
 }
-
