55ae3881fee419c9155519b6bd11df2d82cae764
KayAhn0126 <dkseotn123@gmail.com>
2023-04-19 23:16:42 +0900

    update: 인구 이동 문제 다시 풀기 업데이트

--- Re-Solve/Re-Solve/PopulationMovement/main.swift
+++ Re-Solve/Re-Solve/PopulationMovement/main.swift
@@ -1,19 +1,4 @@
-//
-//  main.swift
-//  PopulationMovement
-//
-//  Created by Kay on 2023/03/06.
-//
-
-/*
- 16234
- 인구이동
- */
-
 import Foundation
-
-let dy = [-1,0,1,0]
-let dx = [0,1,0,-1]
 
 let NLR = readLine()!.split(separator: " ").map { Int(String($0))! }
 let N = NLR[0]
@@ -21,57 +6,62 @@ let L = NLR[1]
 let R = NLR[2]
 
 var adjMatrix = [[Int]](repeating: [Int](), count: N)
-//visited는 반복할때마다.
-
 for i in 0..<N {
     adjMatrix[i].append(contentsOf: readLine()!.split(separator: " ").map { Int(String($0))! })
 }
 
+let dy = [-1,0,1,0]
+let dx = [0,1,0,-1]
 
-var sumList: [(Int,Int)] = []
-var result = 0
+var changeList: [(Int, Int)] = []
+
+var visited = [[Bool]](repeating: [Bool](repeating: false, count: N), count: N)
+var tempResult = 0
+func dfs(_ y: Int, _ x: Int) {
+    for i in 0..<4 {
+        let ny = y + dy[i]
+        let nx = x + dx[i]
+        if ny < 0 || nx < 0 || ny >= N || nx >= N { continue }
+        if visited[ny][nx] == true { continue }
+        let between = abs(adjMatrix[y][x] - adjMatrix[ny][nx])
+        if L <= between && between <= R {
+            changeList.append((ny,nx))
+            tempResult += adjMatrix[ny][nx]
+            visited[ny][nx] = true
+            dfs(ny,nx)
+        }
+    }
+}
+
+func visitedClear() {
+    visited = [[Bool]](repeating: [Bool](repeating: false, count: N), count: N)
+}
+
+
+var time = 0
 while true {
-    var needMoreMoveFlag = false
-    var visited = [[Int]](repeating: [Int](repeating: 0, count: N), count: N)
+    var noChangeFlag = false
     for i in 0..<N {
         for j in 0..<N {
-            if visited[i][j] == 0 {
-                var bfsQueue = [(Int,Int)](), idx = 0
-                bfsQueue.append((i,j))
-                visited[i][j] = 1
-                sumList.append((i,j))
-                var populationTotal = adjMatrix[i][j]
-                while idx < bfsQueue.count {
-                    let currentLocation = bfsQueue[idx]; idx += 1
-                    let currentY = currentLocation.0
-                    let currentX = currentLocation.1
-                    for i in 0..<4 {
-                        let ny = currentY + dy[i]
-                        let nx = currentX + dx[i]
-                        if ny < 0 || nx < 0 || ny >= N || nx >= N { continue }
-                        if visited[ny][nx] > 0 { continue }
-                        if L <= abs(adjMatrix[currentY][currentX] - adjMatrix[ny][nx]) && abs(adjMatrix[currentY][currentX] - adjMatrix[ny][nx]) <= R {
-                            sumList.append((ny,nx))
-                            populationTotal += adjMatrix[ny][nx]
-                            visited[ny][nx] = 1
-                            bfsQueue.append((ny,nx))
-                        }
-                    }
-                }
-                let sumListCount = sumList.count
-                if sumListCount > 1 {
-                    sumList.enumerated().forEach {
-                        adjMatrix[$0.element.0][$0.element.1] = populationTotal / sumListCount
-                    }
-                    needMoreMoveFlag = true
-                }
-                sumList.removeAll()
+            if visited[i][j] == true { continue }
+            changeList.removeAll()
+            tempResult = 0
+            visited[i][j] = true
+            changeList.append((i,j))
+            tempResult += adjMatrix[i][j]
+            dfs(i,j)
+            if changeList.count == 1 { continue }
+            let count = changeList.count
+            for i in 0..<count {
+                adjMatrix[changeList[i].0][changeList[i].1] = tempResult / count
+                noChangeFlag = true
             }
         }
     }
-    if needMoreMoveFlag == false {
+    if noChangeFlag == false {
         break
     }
-    result += 1
+    visitedClear()
+    time += 1
 }
-print(result)
+print(time)
