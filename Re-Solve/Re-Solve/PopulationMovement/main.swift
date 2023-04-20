2dcb3de80378d407bfbc375130fd3ba943c575cb
KayAhn0126 <dkseotn123@gmail.com>
2023-04-21 00:29:19 +0900

    update: 인구이동 다시 풀기 업데이트

--- Re-Solve/Re-Solve/PopulationMovement/main.swift
+++ Re-Solve/Re-Solve/PopulationMovement/main.swift
@@ -5,63 +5,58 @@ let N = NLR[0]
 let L = NLR[1]
 let R = NLR[2]
 
-var adjMatrix = [[Int]](repeating: [Int](), count: N)
+var board = [[Int]](repeating: [Int](), count: N)
+
 for i in 0..<N {
-    adjMatrix[i].append(contentsOf: readLine()!.split(separator: " ").map { Int(String($0))! })
+    board[i].append(contentsOf: readLine()!.split(separator: " ").map { Int(String($0))! })
 }
 
 let dy = [-1,0,1,0]
 let dx = [0,1,0,-1]
-
-var changeList: [(Int, Int)] = []
-
 var visited = [[Bool]](repeating: [Bool](repeating: false, count: N), count: N)
-var tempResult = 0
+var unionList: [(Int,Int)] = []
+var unionSum = 0
+
 func dfs(_ y: Int, _ x: Int) {
+    visited[y][x] = true
     for i in 0..<4 {
         let ny = y + dy[i]
         let nx = x + dx[i]
         if ny < 0 || nx < 0 || ny >= N || nx >= N { continue }
-        if visited[ny][nx] == true { continue }
-        let between = abs(adjMatrix[y][x] - adjMatrix[ny][nx])
+        if visited[ny][nx] != false { continue }
+        let between = abs(board[y][x] - board[ny][nx])
         if L <= between && between <= R {
-            changeList.append((ny,nx))
-            tempResult += adjMatrix[ny][nx]
-            visited[ny][nx] = true
+            unionList.append((ny,nx))
+            unionSum += board[ny][nx]
             dfs(ny,nx)
         }
     }
 }
 
-func visitedClear() {
-    visited = [[Bool]](repeating: [Bool](repeating: false, count: N), count: N)
-}
-
-
 var time = 0
 while true {
-    var noChangeFlag = false
+    var flag = false
+    visited = [[Bool]](repeating: [Bool](repeating: false, count: N), count: N)
     for i in 0..<N {
         for j in 0..<N {
             if visited[i][j] == true { continue }
-            changeList.removeAll()
-            tempResult = 0
-            visited[i][j] = true
-            changeList.append((i,j))
-            tempResult += adjMatrix[i][j]
+            unionList.removeAll()
+            unionSum = 0
+            unionList.append((i,j))
+            unionSum += board[i][j]
             dfs(i,j)
-            if changeList.count == 1 { continue }
-            let count = changeList.count
-            for i in 0..<count {
-                adjMatrix[changeList[i].0][changeList[i].1] = tempResult / count
-                noChangeFlag = true
+            if unionList.count != 1 {
+                let num = unionList.count
+                for loc in unionList {
+                    board[loc.0][loc.1] = unionSum / num
+                }
+                flag = true
             }
         }
     }
-    if noChangeFlag == false {
+    if flag == false {
         break
     }
-    visitedClear()
     time += 1
 }
 print(time)
