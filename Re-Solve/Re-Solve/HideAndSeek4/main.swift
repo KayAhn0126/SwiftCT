b8311a10a230e6f89043a9b9b4bfc6a149a45f84
KayAhn0126 <dkseotn123@gmail.com>
2023-04-24 23:29:48 +0900

    update: 숨바꼭질4 문제 다시 풀기 업데이트

--- Re-Solve/Re-Solve/HideAndSeek4/main.swift
+++ Re-Solve/Re-Solve/HideAndSeek4/main.swift
@@ -1,65 +1,53 @@
-//
-//  main.swift
-//  HideAndSeek4
-//
-//  Created by Kay on 2023/03/28.
-//
-
-/*
- 13913
- 숨바꼭질4 다시 풀기
- */
 import Foundation
 
 let NK = readLine()!.split(separator: " ").map { Int(String($0))! }
-let N = NK[0] // 수빈이의 위치
-let K = NK[1] // 동생의 위치
+let N = NK[0]
+let K = NK[1]
 
+// trace 문제
 
 var visited = [Int](repeating: 0, count: 100001)
-var prev = [Int](repeating: -1, count: 100001)
+var trace = [Int](repeating: 0, count: 100001)
 
 
 var bfsQueue = [Int](), idx = 0
-visited[N] = 1
 bfsQueue.append(N)
+visited[N] = 1
 
+var result = 0
+let walk = [-1,1,2]
 while idx < bfsQueue.count {
-    let currentNumber = bfsQueue[idx]; idx += 1
-    if currentNumber == K {
+    let current = bfsQueue[idx]; idx += 1
+    if current == K {
+        result = visited[current] - 1
         break
     }
-    for i in 1...3 {
-        var nextNumber = currentNumber
-        if i == 1 {
-            nextNumber -= 1
-        } else if i == 2 {
-            nextNumber += 1
+    for i in 0..<3 {
+        var next = 0
+        if i == 0 || i == 1 {
+            next = current + walk[i]
         } else {
-            nextNumber *= 2
+            next = current * walk[i]
         }
-        if nextNumber < 0 || nextNumber > 100000 { continue }
-        if visited[nextNumber] > 0 { continue }
-        bfsQueue.append(nextNumber)
-        visited[nextNumber] = visited[currentNumber] + 1
-        prev[nextNumber] = currentNumber
+        if next < 0 || next >= 100001 { continue }
+        if visited[next] != 0 { continue }
+        if trace[next] != 0 { continue }
+        visited[next] = visited[current] + 1
+        trace[next] = current
+        bfsQueue.append(next)
     }
 }
 
-var result = [Int]()
-if N == K {
-    print(0)
-    print(N)
-} else {
-    print(visited[K] - 1)
-    var traceBackNumber = prev[K]
-    result.append(K)
-    while traceBackNumber != -1 {
-        result.append(traceBackNumber)
-        traceBackNumber = prev[traceBackNumber]
-    }
-    result.reversed().enumerated().forEach {
-        print($0.element, terminator: " ")
-    }
-    print("")
+var resultArr = [Int]()
+var num = K
+resultArr.append(K)
+while num != N {
+    let temp = trace[num]
+    resultArr.append(temp)
+    num = temp
 }
+print(result)
+resultArr.reversed().enumerated().forEach {
+    print($0.element, terminator: " ")
+}
+print("")
