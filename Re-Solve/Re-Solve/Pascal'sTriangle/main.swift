4923d8d131ce2c093c765186161259213b4bce72
KayAhn0126 <dkseotn123@gmail.com>
2023-04-27 01:02:01 +0900

    update: 파스칼의 삼각형 문제 다시 풀기 업데이트

--- Re-Solve/Re-Solve/Pascal'sTriangle/main.swift
+++ Re-Solve/Re-Solve/Pascal'sTriangle/main.swift
@@ -1,29 +1,24 @@
-//
-//  main.swift
-//  Pascal'sTriangle
-//
-//  Created by Kay on 2023/03/29.
-//
-
 import Foundation
 
 let NK = readLine()!.split(separator: " ").map { Int(String($0))! }
 let N = NK[0]
 let K = NK[1]
 
-//30줄까지 총 30 * 30
-var adjMatrix = [[Int]](repeating: [Int](repeating: 0, count: 31), count: 31)
-adjMatrix[1][1] = 1
-adjMatrix[2][1] = 1
-adjMatrix[2][2] = 1
-for i in 3...30 {
-    adjMatrix[i][1] = 1
-    adjMatrix[i][i] = 1
-}
+var arr = [[Int]](repeating: [Int](repeating: 0, count: 31), count: 31)
 
-for i in 3...30 {
-    for j in 2..<i {
-        adjMatrix[i][j] = adjMatrix[i-1][j-1] + adjMatrix[i-1][j]
+arr[1][1] = 1
+arr[2][1] = 1
+arr[2][2] = 1
+
+if N < 3 {
+    print(arr[N][K])
+} else {
+    for i in 3...N {
+        arr[i][1] = 1
+        arr[i][i] = 1
+        for j in 2..<i {
+            arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
+        }
     }
+    print(arr[N][K])
 }
-print(adjMatrix[N][K])
