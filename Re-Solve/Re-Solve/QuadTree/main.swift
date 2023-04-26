58621366bb10fddebf16726040e4842cb6f21871
KayAhn0126 <dkseotn123@gmail.com>
2023-04-26 20:16:28 +0900

    update: 쿼드트리 문제 다시 풀기 업데이트

--- Re-Solve/Re-Solve/QuadTree/main.swift
+++ Re-Solve/Re-Solve/QuadTree/main.swift
@@ -1,46 +1,34 @@
-//
-//  main.swift
-//  QuadTree
-//
-//  Created by Kay on 2023/02/07.
-//
-
-/*
- 1992
- 쿼드트리
- */
 import Foundation
 
 let N = Int(readLine()!)!
 
-var matrix = [[Character]](repeating: [Character](), count: N)
+var adjMatrix = [[Character]](repeating: [Character](), count: N)
 
 for i in 0..<N {
-    matrix[i].append(contentsOf: readLine()!)
+    adjMatrix[i].append(contentsOf: readLine()!)
 }
 
-
-func calculate(_ y: Int, _ x: Int, _ size: Int) -> String {
-    let current = matrix[y][x]
-    if size == 1 {
-        return String(current)
-    }
+//MARK: 재귀는 기저사례가 있어야 한다!
+func getPressedResult(_ size: Int, _ y: Int, _ x: Int) -> String {
     var result = ""
+    var currentLetter = adjMatrix[y][x]
+    if size == 1 {
+        return String(currentLetter)
+    }
     for i in y..<y+size {
         for j in x..<x+size {
-            if matrix[i][j] != current {
+            if currentLetter != adjMatrix[i][j] {
                 result += "("
-                result += calculate(y, x, size/2)
-                result += calculate(y, x + size/2, size/2)
-                result += calculate(y + size/2, x, size/2)
-                result += calculate(y + size/2, x + size/2, size/2)
+                result += getPressedResult(size/2, y, x)
+                result += getPressedResult(size/2, y, x + size/2)
+                result += getPressedResult(size/2, y + size/2, x)
+                result += getPressedResult(size/2, y + size/2, x + size/2)
                 result += ")"
                 return result
             }
         }
     }
-    return String(current)
+    return String(currentLetter)
 }
 
-print(calculate(0,0,N))
-
+print(getPressedResult(N,0,0))
