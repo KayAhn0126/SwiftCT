eec3e371cf8fa914f65f9b3843862075fea81a81
KayAhn0126 <dkseotn123@gmail.com>
2023-04-27 18:23:16 +0900

    update: 오큰수 문제 다시 풀기 업데이트

--- Re-Solve/Re-Solve/RightBiggerNumber/main.swift
+++ Re-Solve/Re-Solve/RightBiggerNumber/main.swift
@@ -1,20 +1,11 @@
-//
-//  main.swift
-//  RightBiggerNumber
-//
-//  Created by Kay on 2023/02/11.
-//
-
-/*
- 17298
- 오큰수
- */
-
 import Foundation
+
+let number = Int(readLine()!)!
+
+var arr = readLine()!.split(separator: " ").map { Int(String($0))! }
 
 struct Stack<T> {
     var elements: [T] = []
-    
     var count: Int {
         return elements.count
     }
@@ -36,28 +27,21 @@ struct Stack<T> {
     }
 }
 
-let number = Int(readLine()!)!
-
-var userInput = readLine()!.split(separator: " ").map { Int(String($0))! }
+var myStack = Stack<Int>()
 
 var result = [Int](repeating: -1, count: number)
 
-var myStack = Stack<Int>()
+// arr를 돌면서 (empty라면 stack에 push) 또는! (현재 값이 top보다 작으면 push)
+// 현재 값이 stack의 top보다 크면 해당 인덱스를 현재 값으로 채운다. until 현재 값이 같거나 큰 동안만 계속 pop한다.
 
-// 1. inputArr을 돈다.
-// 2. 현재 stack이 비어있거나 현재 수가 top에 있는 수보다 작으면 넣어준다. (넣어줄때 인덱스를 넣어준다.)
-// 3. 비어있지 않고, 현재 수가 top보다 크면 현재수가 계속 클 상황까지 pop을 해준다.
-// 3-1. while stack.top() < inputArr[currentIndex] {
-//  stack에 있는 요소(인덱스)를 pop해주고 result에서 해당 인덱스의 값을 바꿔준다.
-// }
 
 for i in 0..<number {
-    if myStack.isEmpty || userInput[myStack.top!] > userInput[i] {
+    if myStack.isEmpty || arr[myStack.top!] > arr[i] {
         myStack.push(i)
         continue
     }
-    while !myStack.isEmpty && userInput[myStack.top!] < userInput[i] {
-        result[myStack.pop()!] = userInput[i]
+    while !myStack.isEmpty && arr[myStack.top!] < arr[i] {
+        result[myStack.pop()!] = arr[i]
     }
     myStack.push(i)
 }
@@ -65,5 +49,4 @@ for i in 0..<number {
 result.enumerated().forEach {
     print(String($0.element), terminator: " ")
 }
-
-// 40분 정도
+print("")
