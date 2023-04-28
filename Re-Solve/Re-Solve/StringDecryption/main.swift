938f8121eff249cb5df17636c8c0ca0e06dd6a0f
KayAhn0126 <dkseotn123@gmail.com>
2023-04-28 23:02:25 +0900

    update: 문자 해독 문제 다시 풀기 업데이트

--- Re-Solve/Re-Solve/StringDecryption/main.swift
+++ Re-Solve/Re-Solve/StringDecryption/main.swift
@@ -1,55 +1,58 @@
 import Foundation
 
-extension String {
-    subscript(idx: Int) -> Character? {
-        guard(0..<count).contains(idx) else { return nil }
-        let target = index(startIndex, offsetBy: idx)
-        return self[target]
+//extension String {
+//    subscript(idx: Int) -> Character? {
+//        guard(0..<count).contains(idx) else { return nil }
+//        let target = index(startIndex, offsetBy: idx)
+//        return self[target]
+//    }
+//}
+
+let wlsl = readLine()!.split(separator: " ").map { Int(String($0))! }
+let wl = wlsl[0] // W의 길이
+let sl = wlsl[1] // S의 길이
+
+// 대문자 먼저(0~25), 소문자 나중(26~51)
+var wArr = [Int](repeating: 0, count: 52)
+
+let W = readLine()!.map { Character(String($0))} // 문자열 W
+let S = readLine()!.map { Character(String($0))} // 문자열 S
+
+for letter in W {
+    let currentLetterNum = Int(letter.asciiValue!)
+    if 65 <= currentLetterNum && currentLetterNum <= 90 {
+        wArr[currentLetterNum - 65] += 1
+    } else {
+        wArr[currentLetterNum - 97 + 26] += 1
     }
 }
 
-let NL = readLine()!.split(separator: " ").map { Int(String($0))! }
-let N = NL[0]
-let L = NL[1]
-
-let find = readLine()!.map { Character(String($0)) }
-let given = readLine()!.map { Character(String($0)) }
-
-var findVisited = [Int](repeating: 0, count: 53)
-var givenVisited = [Int](repeating: 0, count: 53)
-
-for i in find {
-    let number = Int(i.asciiValue!)
-    if 65 <= number && number <= 90 {
-        findVisited[number - 65] += 1
-    } else if 97 <= number && number <= 122 {
-        findVisited[number - 97 + 26] += 1
-    }
-}
-
-var count = 0
+var sArr = [Int](repeating: 0, count: 52)
 var result = 0
-var flag = false
+var currentCount = 0
 
-for i in 0..<L {
-    let number = Int(given[i].asciiValue!)
-    if 65 <= number && number <= 90 {
-        givenVisited[number - 65] += 1
-    } else if 97 <= number && number <= 122 {
-        givenVisited[number - 97 + 26] += 1
+// currentCount가 wl이 될때까지 계속 넣어준다.
+// 배열이 같은지 비교한다.
+// 4번전의 character 지워주고 다음올 character 올려주고 배열이 같은지 비교
+for i in 0..<S.count {
+    let currentLetterNum = Int(S[i].asciiValue!)
+    if 65 <= currentLetterNum && currentLetterNum <= 90 {
+        sArr[currentLetterNum - 65] += 1
+    } else {
+        sArr[currentLetterNum - 97 + 26] += 1
     }
-    count += 1
-    if count == N {
-        if findVisited == givenVisited {
+    currentCount += 1
+    if currentCount == wl {
+        if wArr == sArr {
             result += 1
         }
-        let lastNumber = Int(given[i - (N - 1)].asciiValue!)
-        if 65 <= lastNumber && lastNumber <= 90 {
-            givenVisited[lastNumber - 65] -= 1
-        } else if 97 <= lastNumber && lastNumber <= 122 {
-            givenVisited[lastNumber - 97 + 26] -= 1
+        let currentLetterNum = Int(S[i - (wl - 1)].asciiValue!)
+        if 65 <= currentLetterNum && currentLetterNum <= 90 {
+            sArr[currentLetterNum - 65] -= 1
+        } else {
+            sArr[currentLetterNum - 97 + 26] -= 1
         }
-        count -= 1
+        currentCount -= 1
     }
 }
 print(result)
