d0c0ccd2494b2059ab970e0d37a145911cdb3be8
KayAhn0126 <dkseotn123@gmail.com>
2023-04-29 16:32:11 +0900

    update: 주석이 포함된 코드로 업데이트

--- BruteForce/RemoteControl/RemoteControl/RemoteControl/main.swift
+++ BruteForce/RemoteControl/RemoteControl/RemoteControl/main.swift
@@ -11,77 +11,79 @@
  */
 
 /*
- 채널의 숫자가 작아서 브루트포스 가능!
- 고장난 버튼이 없다 -> Length 반환
- 
- 생각할 것
- 1. 고장난 버튼이 없을수 있다.
- 
- 
- 1. 먼저 +,-로 갔을때 100에서 얼마나 눌러야 하는지 구한다 Number - 100 = ??
- 2. 고장나지 않은 유효한 숫자들을 담은 배열을 돌면서 기존 Number와 해당 요소의 절대값이 가장 적은것을 선택해
- 특정 숫자를 만들고 Number - 숫자 해준다.
- 3. 브루트포스
- */
-
-
-/*
  
  A 배열 = [0,1,2,3,4,5,6,7,8,9]
  B 배열 = [0,1,2,3]
  A배열에서 B배열이 가지고 있는 값을 제외한 값을 A배열에 넣고 싶다면..
  A = A.filter { !B.contains($0) }
  print(A) -> [4,5,6,7,8,9]
- 
  */
 
-/*
- 왜 100만인지?
- 시간 복잡도 -> 100만 * len("500,000") = 600만정도 2초면 충분
- */
 
 import Foundation
 
-let numberString = readLine()!
-let number = Int(numberString)!
+let targetNumberInString = readLine()!
+let targetNumber = Int(targetNumberInString)!
+
 let brokenCount = Int(readLine()!)!
-var validArr = [Bool](repeating: true, count: 10)
-var result = abs(number - 100)
+var result = abs(targetNumber - 100) // 100에서 타겟 넘버까지 +,- 버튼을 누른 횟수
 
+var brokenArr = [Bool](repeating: false, count: 10) // 고장난 버튼이 어떤 버튼인지 알 수 있도록 담아두는 배열
 
-func checkValidNumber(_ number: Int) -> Bool {
-    var result = true
-    var testNumber = number
-    
-    if testNumber > 9 {
-        while testNumber > 0 {
-            if validArr[testNumber % 10] == false {
-                result = false
-                break
+//MARK: 랜덤한 브루트포스의 숫자가 고장난 키를 가지고 있는지 확인하는 함수
+func checkNumber(_ num: Int) -> Bool {
+    var flag = true
+    var testNum = num
+    // 원래는 while testNum > 0으로 고장난 키패드를 포함하는지 확인했다.
+    // 하지만 분기 처리한 이유는 0이 들어왔을때 while testNum > 0 으로 한다면 0을 처리하지 못한다.
+    if testNum > 9 {
+        while testNum > 0 {
+            if brokenArr[testNum % 10] == true {
+                flag = false
             }
-            testNumber /= 10
+            testNum /= 10
         }
     } else {
-        if validArr[testNumber] == false {
-            result = false
+        if brokenArr[testNum] == true {
+            flag = false
         }
     }
-    return result
+    
+    return flag
 }
 
-if brokenCount != 0 {
-    let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
-    for i in 0..<arr.count {
-        validArr[arr[i]] = false
+if brokenCount == 0 {
+    //고장난 버튼이 하나도 없다면, 타겟 넘버를 직접 누르는 것과 100에서 +,- 버튼을 눌러 타겟넘버에 도달하는 것 중 작은것을 출력하면 된다.
+    result = result > targetNumberInString.count ? targetNumberInString.count : result
+} else {
+    // 고장난 버튼이 있다면,
+    let tempArr = readLine()!.split(separator: " ").map { Int(String($0))! }
+    for i in tempArr {
+        brokenArr[i] = true
     }
-    for i in 0..<1000000 {
-        if checkValidNumber(i) == true {
-            let digitCount = String(i).count
-            let currentMin = abs(number - i) + digitCount
-            result = result > currentMin ? currentMin : result
+    for i in 0...1000000 {
+        if checkNumber(i) == true { // 현재 숫자 i가 고장난 키를 포함하고 있지 않다면
+            // i를 누르기 위해서 버튼을 클릭한 횟수와 + 이제 i에서 target 숫자까지 도달하기 위한 +,- 버튼을 누른 숫자를 더한다.
+            let tempResult = String(i).count + abs(targetNumber - i)
+            
+            // 이미 맨 위에서 result에 100에서 target 숫자까지 +,- 버튼만으로 가는 횟수를 구했다.
+            // tempResult가 (100에서 타켓까지 +,-로만 이동할 때의 숫자)보다 작으면 갱신을 시켜준다.
+            
+            result = result > tempResult ? tempResult : result
+            
+            // 예를 들어 타겟 넘버가 150이고 고장난 숫자 1개 -> 9라고 해보자
+            // 이미 맨 위에서 result에 100에서 target 숫자까지 +,- 버튼만으로 가는 횟수를 구했다.
+            // 즉 현재 result는 50이다 -> 100에서 50번만 +를 누르면 되기 때문에.
+            // 이제 result보다 작은 값이 있는지 확인해보자!
+            // i = 0, (0을 입력해야 하므로 1) + (0에서 타겟넘버인 150까지 + 버튼을 150번 눌러야 하므로 150) = 151
+            // 151은 50보다 크니까 최소값이 될 수 없다.
+            // i = 1, (1을 입력해야 하므로 1) + (1에서 타겟넘버인 150까지 + 버튼을 149번 눌러야 하므로 149) = 150
+            // ...
+            // i = 10, (10을 입력해야 하므로 2) + (10에서 타겟넘버인 150까지 + 버튼을 140번 눌러야 하므로 140) = 142
+            // i = 100, (100을 입력해야 하므로 3) + (100에서 타겟넘버인 150까지 + 버튼을 50번 눌러야 하므로 50) = 53
+            // i = 150, (150을 입력해야 하므로 3) + (150에서 타겟넘버인 150까지 아무것도 안눌러도 되므로 0) = 3
+            // 이 경우 답은 가장 작은 숫자인 3이 된다.
         }
     }
-} else {
-    result = result > numberString.count ? numberString.count : result
 }
 print(result)
