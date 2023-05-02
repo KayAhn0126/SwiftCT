d8b15a5ae4a873d66f90764159686b2d68b8926c
KayAhn0126 <dkseotn123@gmail.com>
2023-05-02 19:04:48 +0900

    update: 1로 만들기 문제 다시 풀기 업데이트

--- Re-Solve/Re-Solve/MakeToOne/main.swift
+++ Re-Solve/Re-Solve/MakeToOne/main.swift
@@ -1,32 +1,26 @@
-//
-//  main.swift
-//  MakeToOne
-//
-//  Created by Kay on 2023/03/17.
-//
-
-/*
- 1463
- 1로 만들기 다시풀기
- */
-
 import Foundation
 
-var number = Int(readLine()!)!
-
+var num = Int(readLine()!)!
 var dp = [Int](repeating: 0, count: 1000001)
-dp[1] = 0
-dp[2] = 1
-dp[3] = 1
 
-for i in 4...1000000 {
-    dp[i] = dp[i-1] + 1
-    if i % 2 == 0 {
-        dp[i] = min(dp[i / 2] + 1, dp[i])
+if num == 1 {
+    print(0)
+} else if num == 2 || num == 3 {
+    print(1)
+} else {
+    dp[1] = 0
+    dp[2] = 1
+    dp[3] = 1
+
+    for i in 4..<1000001 {
+        dp[i] = dp[i-1] + 1
+        if i % 2 == 0 {
+            dp[i] = min(dp[i], dp[i/2] + 1)
+        }
+        if i % 3 == 0 {
+            dp[i] = min(dp[i], dp[i/3] + 1)
+        }
     }
-    if i % 3 == 0 {
-        dp[i] = min(dp[i / 3] + 1, dp[i])
-    }
+    print(dp[num])
 }
-print(dp[number])
 
