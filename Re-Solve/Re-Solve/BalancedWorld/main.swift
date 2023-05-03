4f8d2640d864887ab00a992fb2aa3ddd3b5f95a7
KayAhn0126 <dkseotn123@gmail.com>
2023-05-03 12:47:43 +0900

    update: 균형잡힌 세상 문제 다시 풀기 업데이트

--- Re-Solve/Re-Solve/BalancedWorld/main.swift
+++ Re-Solve/Re-Solve/BalancedWorld/main.swift
@@ -1,25 +1,7 @@
-//
-//  main.swift
-//  BalancedWorld
-//
-//  Created by Kay on 2023/02/12.
-//
-
-/*
- 4949
- 균형잡힌 세상
- */
-
-/*
- () []
- 1:1 매칭
- 
- */
 import Foundation
 
 struct Stack<T> {
     var elements: [T] = []
-    
     var count: Int {
         return elements.count
     }
@@ -35,42 +17,33 @@ struct Stack<T> {
     mutating func push(_ element: T) {
         elements.append(element)
     }
-    
     mutating func pop() -> T? {
         return elements.popLast()
     }
 }
 
-while let testString = readLine() {
-    var str = testString
-    if str.last! == "." {
-        if str.first! == str.last! {
-            break
-        }
-        var myStack = Stack<Character>()
-        for i in str {
-            if !myStack.isEmpty {
-                if (myStack.top! == "(" && i == ")") || (myStack.top! == "[" && i == "]") {
-                    myStack.pop()
-                } else {
-                    if i == "(" || i == "[" || i == ")" || i == "]" {
-                        myStack.push(i)
-                    }
-                }
-            } else {
-                if i == "(" || i == "[" || i == ")" || i == "]" {
-                    myStack.push(i)
-                }
-            }
-        }
+while let given = readLine() {
+    let myString = given
+    if myString.count == 1 && myString == "." {
+        break
+    }
+    var myStack = Stack<Character>()
+    for char in myString {
+        if char != "(" && char != ")" && char != "[" && char != "]" { continue }
         if myStack.isEmpty {
-            print("yes")
+            myStack.push(char)
+        } else if !myStack.isEmpty && myStack.top! == "(" && char == ")" {
+            myStack.pop()
+        } else if !myStack.isEmpty && myStack.top! == "[" && char == "]" {
+            myStack.pop()
         } else {
-            print("no")
+            myStack.push(char)
         }
+    }
+    
+    if myStack.isEmpty {
+        print("yes")
     } else {
         print("no")
     }
 }
-
-// 40분
