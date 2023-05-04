9d265b31e03f7ddbe8179808d4e1bae0297c8558
KayAhn0126 <dkseotn123@gmail.com>
2023-05-04 15:46:24 +0900

    add: 숫자 카드 2 문제 다시 풀기 추가

---
+++ Re-Solve/Re-Solve/NumberCard2/main.swift
@@ -0,0 +1,87 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let fIO = FileIO()
+
+let N = fIO.readInt()
+var arr = [Int](repeating: 0, count: 20000001)
+
+for _ in 0..<N {
+    let tempNum = fIO.readInt()
+    arr[tempNum + 10000000] += 1
+}
+
+let M = fIO.readInt()
+var sArr = [Int](repeating: 0, count: 500001)
+
+var resultArr = [Int]()
+
+for _ in 0..<M {
+    let tempNum = fIO.readInt()
+    resultArr.append(arr[tempNum + 10000000])
+}
+
+resultArr.enumerated().forEach {
+    print($0.element, terminator: " ")
+}
+print("")
+
+
+
