66898bd6ac76df348a5311d3cd1eb4a6154add19
KayAhn0126 <dkseotn123@gmail.com>
2023-05-04 18:46:10 +0900

    update: 과자 나눠주기 문제 다시 풀기 업데이트

--- Re-Solve/Re-Solve/SharingSnack/main.swift
+++ Re-Solve/Re-Solve/SharingSnack/main.swift
@@ -1,49 +1,34 @@
-//
-//  main.swift
-//  SharingSnack
-//
-//  Created by Kay on 2023/03/25.
-//
-
-/*
- 16401
- 과자 나눠주기 다시 풀기
- */
-
 import Foundation
 
-let MN = readLine()!.split(separator: " ").map { Int(String($0))! }
-let M = MN[0] // 사람 숫자
-let N = MN[1] // 과자의 수
+let NM = readLine()!.split(separator: " ").map { Int(String($0))! }
+let N = NM[0] // number of cousin
+let M = NM[1] // number of snack
 
 var arr = readLine()!.split(separator: " ").map { Int(String($0))! }
 
-
-//MARK: 배열을 돌면서 나눈수가 과자의 수와 일치하는지 체크
-func checkIfPossible(_ num: Int) -> Bool {
-    var result = 0
-    for i in 0..<arr.count {
-        result += arr[i] / num
-        if result >= M {
-            return true
-        }
+func isBigEnough(_ num: Int) -> Bool {
+    var temp = 0
+    for element in arr {
+        if element < num { continue }
+        temp += (element / num)
+        if temp >= N { return true }
     }
     return false
 }
 
-//MARK: Binary Search
-var start = 1 // checkIfPossible 함수에서 매개변수로 받아온 값을 나누니, 0으로 두면 런타임 에어 발생. 
-var end = 1000000000
-var mid = 0
-
-var result = 0
-while start <= end {
-    mid = (start + end) / 2
-    if checkIfPossible(mid) == true {
-        result = mid
-        start = mid + 1
-    } else {
-        end = mid - 1
+func binarySearch() -> Int {
+    var result = 0
+    var start = 1
+    var end = 1000000000, mid = 0
+    while start <= end {
+        mid = (start + end) / 2
+        if isBigEnough(mid) == true {
+            result = mid
+            start = mid + 1
+        } else {
+            end = mid - 1
+        }
     }
+    return result
 }
-print(result)
+print(binarySearch())
