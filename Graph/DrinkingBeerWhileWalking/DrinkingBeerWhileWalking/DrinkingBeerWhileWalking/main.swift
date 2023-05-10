af1eab193a8185f2893cef4469c972364ebbb88f
KayAhn0126 <dkseotn123@gmail.com>
2023-05-10 18:17:36 +0900

    update: 맥주 마시면서 걸어가기 문제 해결 및 업데이트

--- Graph/DrinkingBeerWhileWalking/DrinkingBeerWhileWalking/DrinkingBeerWhileWalking/main.swift
+++ Graph/DrinkingBeerWhileWalking/DrinkingBeerWhileWalking/DrinkingBeerWhileWalking/main.swift
@@ -33,6 +33,8 @@ var tc = Int(readLine()!)!
 
 while tc != 0 {
     tc -= 1
+    
+    //페스티벌 까지 갈 수 있는지 확인하는 플래그 갈 수 있다면 true!
     var happyFlag = false
     let cvStoreCount = Int(readLine()!)!
     let homeLocation = readLine()!.split(separator: " ").map { Int(String($0))! }
@@ -45,20 +47,27 @@ while tc != 0 {
         let location = readLine()!.split(separator: " ").map { Int(String($0))! }
         cvStoreLocation.append((location[1], location[0]))
     }
+    
+    //페스티벌 위치 입력 받기
     let destinationLocation = readLine()!.split(separator: " ").map { Int(String($0))! }
     
     var visited = [Bool](repeating: false, count: cvStoreCount + 1)
+    
+    //BFS 탐색
     while idx < bfsQueue.count {
         let currentLocation = bfsQueue[idx]; idx += 1
-        if abs(destinationLocation[1] - currentLocation.1) + abs(destinationLocation[0] - currentLocation.0) <= 1000 {
+        // 현재 위치에서 페스티벌까지 1000이하라면 갈 수 있다!
+        if abs(currentLocation.0 - destinationLocation[1]) + abs(currentLocation.1 - destinationLocation[0]) <= 1000 {
             happyFlag = true
             break
         }
+        
+        // 위치마다 갈 수 있는지 체크 후 큐에 넣기
         for i in 0..<cvStoreLocation.count {
-            if abs(cvStoreLocation[i].1 - currentLocation.1) + abs(cvStoreLocation[i].0 - currentLocation.0) <= 1000 {
+            if abs(cvStoreLocation[i].0 - currentLocation.0) + abs(cvStoreLocation[i].1 - currentLocation.1) <= 1000 {
                 if visited[i] == true { continue }
                 visited[i] = true
-                bfsQueue.append((cvStoreLocation[i].1, cvStoreLocation[i].0))
+                bfsQueue.append((cvStoreLocation[i].0, cvStoreLocation[i].1))
             }
         }
     }
