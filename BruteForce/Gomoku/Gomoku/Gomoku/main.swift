4182511955110203635665294fd7f72c35913a98
KayAhn0126 <dkseotn123@gmail.com>
2023-05-17 23:01:38 +0900

    modify: 일부 코드 수정
    현재 진행 중

--- BruteForce/Gomoku/Gomoku/Gomoku/main.swift
+++ BruteForce/Gomoku/Gomoku/Gomoku/main.swift
@@ -12,54 +12,93 @@
 
 import Foundation
 
-var adjMatrix = [[Int]](repeating: [Int](), count: 20)
+var adjMatrix = [[Int]](repeating: [Int](), count: 19)
 for i in 0..<19 {
     adjMatrix[i].append(contentsOf: readLine()!.split(separator: " ").map { Int(String($0))! })
 }
-adjMatrix[19].append(contentsOf: [Int](repeating: 0, count: 19))
 
 adjMatrix.enumerated().forEach {
     print($0.element)
 }
 
 func checkStraightBottom(_ y: Int, _ x: Int, _ currentNumber: Int) -> Bool {
+    var result = true
     if adjMatrix[y][x] == currentNumber &&
         adjMatrix[y+1][x] == currentNumber &&
         adjMatrix[y+2][x] == currentNumber &&
         adjMatrix[y+3][x] == currentNumber &&
-        adjMatrix[y+4][x] == currentNumber &&
-        adjMatrix[y+5][x] != currentNumber {
-        return true
+        adjMatrix[y+4][x] == currentNumber {
+        if y == 0 {
+            if adjMatrix[y+5][x] == currentNumber {
+                result = false
+            }
+        } else {
+            if adjMatrix[y-1][x] == currentNumber || adjMatrix[y+5][x] == currentNumber {
+                result = false
+            }
+        }
+    } else {
+        result = false
     }
-    return false
+    return result
 }
 
 func checkStraightDiagonal(_ y: Int, _ x: Int, _ currentNumber: Int) -> Bool {
+    var result = true
     if adjMatrix[y][x] == currentNumber &&
         adjMatrix[y+1][x+1] == currentNumber &&
         adjMatrix[y+2][x+2] == currentNumber &&
         adjMatrix[y+3][x+3] == currentNumber &&
-        adjMatrix[y+4][x+4] == currentNumber &&
-        adjMatrix[y+5][x+5] != currentNumber {
-        return true
+        adjMatrix[y+4][x+4] == currentNumber {
+        if y == 0 || x == 0 {
+            if adjMatrix[y+5][x+5] == currentNumber { result = false }
+        } else {
+            if adjMatrix[y-1][x-1] == currentNumber || adjMatrix[y+5][x+5] == currentNumber { result = false }
+        }
+    } else {
+        result = false
     }
-    return false
+    return result
+}
+
+func checkOtherSideDiagonal(_ y: Int, _ x: Int, _ currentNumber: Int) -> Bool {
+    var result = true
+    if adjMatrix[y][x] == currentNumber &&
+        adjMatrix[y+1][x-1] == currentNumber &&
+        adjMatrix[y+2][x-2] == currentNumber &&
+        adjMatrix[y+3][x-3] == currentNumber &&
+        adjMatrix[y+4][x-4] == currentNumber {
+        if x > 5 {
+            if adjMatrix[y]
+        } else {
+            
+        }
+    } else {
+        result = false
+    }
+    return result
 }
 
 func checkStraightRight(_ y: Int, _ x: Int, _ currentNumber: Int) -> Bool {
+    var result = true
     if adjMatrix[y][x] == currentNumber &&
         adjMatrix[y][x+1] == currentNumber &&
         adjMatrix[y][x+2] == currentNumber &&
         adjMatrix[y][x+3] == currentNumber &&
         adjMatrix[y][x+4] == currentNumber {
-        if x != 0 {
-            if adjMatrix[y][x-1] != currentNumber {
-                return true
+        if x == 0 {
+            if adjMatrix[y][x+5] == currentNumber {
+                result = false
+            }
+        } else {
+            if adjMatrix[y][x-1] == currentNumber || adjMatrix[y][x+5] == currentNumber {
+                result = false
             }
         }
-        return true
+    } else {
+        result = false
     }
-    return false
+    return result
 }
 
 var location: [(Int, Int)] = []
@@ -75,6 +114,7 @@ for i in 0...14 {
         if checkStraightRight(i, j, 2) { location.append((i,j)) }
     }
 }
+
 if adjMatrix[location[0].0][location[0].1] == 1 {
     print(1)
 } else {
