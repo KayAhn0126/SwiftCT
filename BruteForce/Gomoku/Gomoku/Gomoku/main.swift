b0f315f439d00ec04cb4b6ba53d34c23dad60d69
KayAhn0126 <dkseotn123@gmail.com>
2023-05-19 23:52:27 +0900

    update: 오목 문제 코드 수정

--- BruteForce/Gomoku/Gomoku/Gomoku/main.swift
+++ BruteForce/Gomoku/Gomoku/Gomoku/main.swift
@@ -17,10 +17,6 @@ for i in 0..<19 {
     adjMatrix[i].append(contentsOf: readLine()!.split(separator: " ").map { Int(String($0))! })
 }
 
-adjMatrix.enumerated().forEach {
-    print($0.element)
-}
-
 func checkStraightBottom(_ y: Int, _ x: Int, _ currentNumber: Int) -> Bool {
     var result = true
     if adjMatrix[y][x] == currentNumber &&
@@ -32,7 +28,7 @@ func checkStraightBottom(_ y: Int, _ x: Int, _ currentNumber: Int) -> Bool {
             if adjMatrix[y+5][x] == currentNumber {
                 result = false
             }
-        } else {
+        } else if y <= 13 {
             if adjMatrix[y-1][x] == currentNumber || adjMatrix[y+5][x] == currentNumber {
                 result = false
             }
@@ -50,10 +46,14 @@ func checkStraightDiagonal(_ y: Int, _ x: Int, _ currentNumber: Int) -> Bool {
         adjMatrix[y+2][x+2] == currentNumber &&
         adjMatrix[y+3][x+3] == currentNumber &&
         adjMatrix[y+4][x+4] == currentNumber {
-        if y == 0 || x == 0 {
+        if y > 0 && x > 0 {
+            if adjMatrix[y-1][x-1] == currentNumber { result = false }
+        }
+        if y <= 13 && x <= 13 {
             if adjMatrix[y+5][x+5] == currentNumber { result = false }
-        } else {
-            if adjMatrix[y-1][x-1] == currentNumber || adjMatrix[y+5][x+5] == currentNumber { result = false }
+        }
+        if y == 14 && x <= 14 {
+            if adjMatrix[y-1][x-1] == currentNumber  { result = false }
         }
     } else {
         result = false
@@ -64,14 +64,16 @@ func checkStraightDiagonal(_ y: Int, _ x: Int, _ currentNumber: Int) -> Bool {
 func checkOtherSideDiagonal(_ y: Int, _ x: Int, _ currentNumber: Int) -> Bool {
     var result = true
     if adjMatrix[y][x] == currentNumber &&
-        adjMatrix[y+1][x-1] == currentNumber &&
-        adjMatrix[y+2][x-2] == currentNumber &&
-        adjMatrix[y+3][x-3] == currentNumber &&
-        adjMatrix[y+4][x-4] == currentNumber {
-        if x > 5 {
-            if adjMatrix[y]
-        } else {
-            
+        adjMatrix[y-1][x+1] == currentNumber &&
+        adjMatrix[y-2][x+2] == currentNumber &&
+        adjMatrix[y-3][x+3] == currentNumber &&
+        adjMatrix[y-4][x+4] == currentNumber {
+        
+        if y > 4 && x <= 13 {
+            if adjMatrix[y-5][x+5] == currentNumber { result = false }
+        }
+        if y >= 4 && y < 18 && x > 0 && x <= 14 {
+            if adjMatrix[y+1][x-1] == currentNumber { result = false }
         }
     } else {
         result = false
@@ -86,12 +88,13 @@ func checkStraightRight(_ y: Int, _ x: Int, _ currentNumber: Int) -> Bool {
         adjMatrix[y][x+2] == currentNumber &&
         adjMatrix[y][x+3] == currentNumber &&
         adjMatrix[y][x+4] == currentNumber {
-        if x == 0 {
+        if x <= 13 {
             if adjMatrix[y][x+5] == currentNumber {
                 result = false
             }
-        } else {
-            if adjMatrix[y][x-1] == currentNumber || adjMatrix[y][x+5] == currentNumber {
+        }
+        if x > 0 && x <= 14 {
+            if adjMatrix[y][x-1] == currentNumber {
                 result = false
             }
         }
@@ -102,22 +105,35 @@ func checkStraightRight(_ y: Int, _ x: Int, _ currentNumber: Int) -> Bool {
 }
 
 var location: [(Int, Int)] = []
-for i in 0...14 {
-    for j in 0...14 {
+for i in 0..<19 {
+    for j in 0..<19 {
         if adjMatrix[i][j] == 0 { continue }
-        if checkStraightBottom(i, j, 1) { location.append((i,j)) }
-        if checkStraightDiagonal(i, j, 1) { location.append((i,j)) }
-        if checkStraightRight(i, j, 1) { location.append((i,j)) }
-        
-        if checkStraightBottom(i, j, 2) { location.append((i,j)) }
-        if checkStraightDiagonal(i, j, 2) { location.append((i,j)) }
-        if checkStraightRight(i, j, 2) { location.append((i,j)) }
+        if j <= 14 {
+            if checkStraightRight(i, j, 1) { location.append((i,j)) }
+            if checkStraightRight(i, j, 2) { location.append((i,j)) }
+        }
+        if i <= 14 {
+            if checkStraightBottom(i, j, 1) { location.append((i,j)) }
+            if checkStraightBottom(i, j, 2) { location.append((i,j)) }
+        }
+        if i <= 14 && j <= 14 {
+            if checkStraightDiagonal(i, j, 1) { location.append((i,j)) }
+            if checkStraightDiagonal(i, j, 2) { location.append((i,j)) }
+        }
+        if i >= 4 && j <= 14 {
+            if checkOtherSideDiagonal(i,j,1) { location.append((i,j)) }
+            if checkOtherSideDiagonal(i,j,2) { location.append((i,j)) }
+        }
     }
 }
-
-if adjMatrix[location[0].0][location[0].1] == 1 {
-    print(1)
+if location.isEmpty {
+    print(0)
 } else {
-    print(2)
+    if adjMatrix[location[0].0][location[0].1] == 1 {
+        print(1)
+    } else {
+        print(2)
+    }
+    print(location[0].0 + 1, location[0].1 + 1)
 }
-print(location[0].0 + 1, location[0].1 + 1)
+
