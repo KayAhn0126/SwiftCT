dbc246b0deb7603cf0d2f7f0cd9c510c30cc5b4d
KayAhn0126 <dkseotn123@gmail.com>
2023-05-24 23:54:14 +0900

    add: 설탕 배달 문제 해결 및 추가

---
+++ BruteForce/SugarDelivery/SugarDelivery/SugarDelivery/main.swift
@@ -0,0 +1,34 @@
+//
+//  main.swift
+//  SugarDelivery
+//
+//  Created by Kay on 2023/05/24.
+//
+
+/*
+ 2839
+ 설탕 배달
+ */
+
+import Foundation
+
+var n = Int(readLine()!)!
+var result = 0
+
+while true {
+    if n % 5 == 0 {
+        result += (n/5)
+        print(result)
+        break
+    }
+    n -= 3
+    result += 1
+    if n < 0 {
+        print(-1)
+        break
+    }
+}
+
+
+
+
