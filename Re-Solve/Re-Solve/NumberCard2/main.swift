3cb9b4e0be855f3ecf8ee4d2e068c7e222ae82fe
KayAhn0126 <dkseotn123@gmail.com>
2023-06-04 15:56:32 +0900

    resolve: 숫자 카드 2 다시 풀기

--- Re-Solve/Re-Solve/NumberCard2/main.swift
+++ Re-Solve/Re-Solve/NumberCard2/main.swift
@@ -1,87 +1,20 @@
 import Foundation
 
-final class FileIO {
-    private let buffer:[UInt8]
-    private var index: Int = 0
+// 이분 탐색이 필요한가?
+let N = Int(readLine()!)!
 
-    init(fileHandle: FileHandle = FileHandle.standardInput) {
-        
-        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
-    }
+var countArr = [Int](repeating: 0, count: 20000001)
 
-    @inline(__always) private func read() -> UInt8 {
-        defer { index += 1 }
+let currentCards = readLine()!.split(separator: " ").map { Int(String($0))! }
 
-        return buffer[index]
-    }
-
-    @inline(__always) func readInt() -> Int {
-        var sum = 0
-        var now = read()
-        var isPositive = true
-
-        while now == 10
-                || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
-        while now >= 48, now <= 57 {
-            sum = sum * 10 + Int(now-48)
-            now = read()
-        }
-
-        return sum * (isPositive ? 1:-1)
-    }
-
-    @inline(__always) func readString() -> String {
-        var now = read()
-
-        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        let beginIndex = index-1
-
-        while now != 10,
-              now != 32,
-              now != 0 { now = read() }
-
-        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
-    }
-
-    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
-        var now = read()
-
-        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        let beginIndex = index-1
-
-        while now != 10,
-              now != 32,
-              now != 0 { now = read() }
-
-        return Array(buffer[beginIndex..<(index-1)])
-    }
+for element in currentCards {
+    countArr[element + 10000000] += 1
 }
 
-let fIO = FileIO()
+let findNum = Int(readLine()!)!
+let findNumArr = readLine()!.split(separator: " ").map { Int(String($0))! }
 
-let N = fIO.readInt()
-var arr = [Int](repeating: 0, count: 20000001)
-
-for _ in 0..<N {
-    let tempNum = fIO.readInt()
-    arr[tempNum + 10000000] += 1
-}
-
-let M = fIO.readInt()
-var sArr = [Int](repeating: 0, count: 500001)
-
-var resultArr = [Int]()
-
-for _ in 0..<M {
-    let tempNum = fIO.readInt()
-    resultArr.append(arr[tempNum + 10000000])
-}
-
-resultArr.enumerated().forEach {
-    print($0.element, terminator: " ")
+findNumArr.enumerated().forEach {
+    print(countArr[$0.element + 10000000], terminator: " ")
 }
 print("")
-
-
-
