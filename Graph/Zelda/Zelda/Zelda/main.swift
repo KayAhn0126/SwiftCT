e7b6e9b92ae1da14d2e4b37095305b89cbaf9301
KayAhn0126 <dkseotn123@gmail.com>
2023-08-10 23:19:46 +0900

    add: 녹색 옷 입은 애가 젤다지? 문제 추가

---
+++ Graph/Zelda/Zelda/Zelda/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Zelda
+//
+//  Created by Kay on 2023/08/10.
+//
+
+/*
+ 4485
+ 녹색 옷 입은 애가 젤다지?
+ */
+
+
+import Foundation
+
+var result = [String]()
+var index = 1
+while true {
+    let N = Int(readLine()!)!
+    if N == 0 { break }
+    var visited = [[Int]](repeating: [Int](repeating: Int.max, count: N), count: N)
+    
+}
