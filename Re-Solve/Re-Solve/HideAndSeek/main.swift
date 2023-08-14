0cbd4b3d8c389ceaa742a77ae4b30aebad436dcf
KayAhn0126 <dkseotn123@gmail.com>
2023-08-14 15:38:42 +0900

    resolve: 숨바꼭질 문제 다시 풀기 업데이트

--- Re-Solve/Re-Solve/HideAndSeek/main.swift
+++ Re-Solve/Re-Solve/HideAndSeek/main.swift
@@ -1,35 +1,34 @@
-import Foundation
+/*
+ 수빈이의 위치 N
+ 동생의 위치 K
+ N, K 모두 0에서 10만 사이
+ 1초 후에 X - 1, X + 1, X * 2로 갈 수 있다.
+ 가장 빠르게 찾는 법 -> BFS
+ */
 
-// 범위 0부터 100,000까지
+import Foundation
 
 let NK = readLine()!.split(separator: " ").map { Int(String($0))! }
 let N = NK[0]
 let K = NK[1]
 
-var visited = [Int](repeating: 0, count: 100001)
-visited[N] = 1
-
-var walk = [-1,1,2]
-
+// 수빈이의 위치 60000, 동생의 위치가 99999라면 직접 움직이는것보다 순간이동 후 움직이는것이 더 적게 걸린다.
+var visited = [Int](repeating: 0, count: 200001)
 var bfsQueue = [Int](), idx = 0
+visited[N] = 1
 bfsQueue.append(N)
 
 while idx < bfsQueue.count {
     let current = bfsQueue[idx]; idx += 1
-    if current == K {
-        print(visited[current] - 1)
-        break
-    }
+    
+    if current == K { break }
+    let temp = [current - 1, current + 1, current * 2]
     for i in 0..<3 {
-        var next = 0
-        if i == 0 || i == 1 {
-            next = current + walk[i]
-        } else {
-            next = current * walk[i]
-        }
-        if next < 0 || next > 100000 { continue }
-        if visited[next] != 0 { continue }
-        visited[next] = visited[current] + 1
-        bfsQueue.append(next)
+        if temp[i] < 0 || temp[i] > 200000 { continue }
+        if visited[temp[i]] != 0 { continue }
+        visited[temp[i]] = visited[current] + 1
+        bfsQueue.append(temp[i])
     }
 }
+
+print(visited[K] - 1)
