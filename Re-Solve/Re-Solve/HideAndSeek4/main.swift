b0d16e7f25d042c33b276e0e6470188df054d0d3
KayAhn0126 <dkseotn123@gmail.com>
2023-08-14 19:49:28 +0900

    resolve: 숨바꼭질4 다시 풀기 업데이트

--- Re-Solve/Re-Solve/HideAndSeek4/main.swift
+++ Re-Solve/Re-Solve/HideAndSeek4/main.swift
@@ -1,53 +1,64 @@
+/*
+ 지문 해석:
+    수빈이가 동생을 찾는다.
+    최단시간과 어떻게 동생을 찾았는지 경로도 같이 출력해라.
+ 
+ 문제 접근:
+    각 1초이기 때문에 BFS
+    tracking 배열 사용
+    5 = 0
+    10 = 5
+    9 = 10
+    18 = 9
+    17 = 18
+ */
+
 import Foundation
 
 let NK = readLine()!.split(separator: " ").map { Int(String($0))! }
 let N = NK[0]
 let K = NK[1]
 
-// trace 문제
-
-var visited = [Int](repeating: 0, count: 100001)
-var trace = [Int](repeating: 0, count: 100001)
-
+var visited = [Int](repeating: 0, count: 200001)
+var tracking = [Int](repeating: 0, count: 200001)
 
 var bfsQueue = [Int](), idx = 0
 bfsQueue.append(N)
 visited[N] = 1
+tracking[N] = N
 
-var result = 0
-let walk = [-1,1,2]
 while idx < bfsQueue.count {
     let current = bfsQueue[idx]; idx += 1
+    
     if current == K {
-        result = visited[current] - 1
         break
     }
+    
+    let temp = [current - 1, current + 1, current * 2]
     for i in 0..<3 {
-        var next = 0
-        if i == 0 || i == 1 {
-            next = current + walk[i]
-        } else {
-            next = current * walk[i]
-        }
-        if next < 0 || next >= 100001 { continue }
+        let next = temp[i]
+        if next < 0 || next > 200000 { continue }
         if visited[next] != 0 { continue }
-        if trace[next] != 0 { continue }
+        if tracking[next] != 0 { continue }
         visited[next] = visited[current] + 1
-        trace[next] = current
+        tracking[next] = current
         bfsQueue.append(next)
     }
 }
 
-var resultArr = [Int]()
-var num = K
-resultArr.append(K)
-while num != N {
-    let temp = trace[num]
-    resultArr.append(temp)
-    num = temp
+print(visited[K] - 1)
+var prev = K
+var arr: [Int] = [prev]
+if N != K {
+    while true {
+        let x = tracking[prev]
+        if x == N {
+            arr.append(N)
+            break
+        }
+        arr.append(x)
+        prev = x
+    }
 }
-print(result)
-resultArr.reversed().enumerated().forEach {
-    print($0.element, terminator: " ")
-}
-print("")
+var strArr = arr.reversed().map { String($0) }
+print(strArr.joined(separator: " "))
