aba3566a56c0797016de992595eb135ee10541da
KayAhn0126 <dkseotn123@gmail.com>
2023-08-14 17:32:02 +0900

    resolve: 숨바꼭질2 문제 다시풀기 업데이트

--- Re-Solve/Re-Solve/HideAndSeek2/main.swift
+++ Re-Solve/Re-Solve/HideAndSeek2/main.swift
@@ -1,42 +1,36 @@
+/*
+ 숨바꼭질 문제와 비슷, 다만 가장 빠른 시간이 몇 초인지 그리고 몇 가지 방법이 있는지 구해야한다.
+ */
+
 import Foundation
 
 let NK = readLine()!.split(separator: " ").map { Int(String($0))! }
 let N = NK[0]
 let K = NK[1]
 
-var visited = [Int](repeating: 0, count: 100001)
-var waysToFind = [Int](repeating: 0, count: 100001)
-visited[N] = 1
-waysToFind[N] = 1
+var visited = [Int](repeating: 0, count: 200001)
+var waysToGetPoint = [Int](repeating: 0, count: 200001)
 
-let walk = [-1,1,2]
 var bfsQueue = [Int](), idx = 0
 bfsQueue.append(N)
+visited[N] = 1
+waysToGetPoint[N] = 1
 
-if N == K {
-    print(0)
-    print(1)
-} else {
-    while idx < bfsQueue.count {
-        let current = bfsQueue[idx]; idx += 1
-        var next = 0
-        for i in 0..<3 {
-            if i == 0 || i == 1 {
-                next = current + walk[i]
-            } else {
-                next = current * walk[i]
-            }
-            if next < 0 || next > 100000 { continue }
-            if visited[next] == 0 { // next에 현재 최단거리가 들어감
-                visited[next] = visited[current] + 1
-                waysToFind[next] += waysToFind[current]
-                bfsQueue.append(next)
-            } else if visited[next] == visited[current] + 1 {
-                waysToFind[next] += waysToFind[current]
-            }
+while idx < bfsQueue.count {
+    let current = bfsQueue[idx]; idx += 1
+
+    let temp = [current - 1, current + 1, current * 2]
+    for i in 0..<3 {
+        let next = temp[i]
+        if next < 0 || next > 200000 { continue }
+        if visited[next] == 0 { // 처음 도달했다면
+            visited[next] = visited[current] + 1
+            waysToGetPoint[next] += waysToGetPoint[current]
+            bfsQueue.append(next)
+        } else if visited[next] == visited[current] + 1 {
+            waysToGetPoint[next] += waysToGetPoint[current]
         }
     }
-    print(visited[K] - 1)
-    print(waysToFind[K])
 }
-
+print(visited[K] - 1)
+print(waysToGetPoint[K])
